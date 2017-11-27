151cc68ea103b32a8dface81f70bdadecec6f4b8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-27 17:13:14 -0500

    Northpark 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -39,7 +39,7 @@
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <p>{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i> {{}}</p>
+                        <p>{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i></p>
                         
                     </div>
                 </div>
