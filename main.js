9b185e447339051ea42d3c9659be2f2cc9b1df76
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-27 12:52:04 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             changeLocale: function(val) {
                 this.locale = val; // this will update the data store, which in turn will trigger the watcher to update the locale in the system
             },
-            toggleIcon() {
+            menuIcon: function() {
                 this.isOpen = !this.isOpen;
             },
             //this function helps home Page show and hide menu using the prop showMenu
