b7357161c5a30dc6854dc3f2dc122b31ec7a4a01
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-27 16:40:58 -0500

    Northpark 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -2,7 +2,16 @@
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="row">
             <div class="col-md-8">
-                
+                <ul class="menu_content text_center">
+                    <li v-for="hour in hours">
+                        <span class="pull-left col-md-6"> 
+                           {{day_of_the_week(hour.day_of_week)}}
+                        </span>
+                        <span class="pull-right col-md-6 text-left">
+                            {{hour.open_time | moment("h:mma", timezone)}} - {{hour.close_time | moment("h:mma", timezone)}}
+                        </span>
+                    </li>
+                </ul>
             </div>
             <div class="col-md-4">
                 <p>Find Us</p>
@@ -32,8 +41,13 @@
                 
                 timezone () {
                     return this.$store.getters.getTimezone;
-                }
-                
+                },
+                hours(){
+                    var hours = 
+                        _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 })
+                        console.log(hours);
+                        return hours;
+                },
             }
         });
     });
