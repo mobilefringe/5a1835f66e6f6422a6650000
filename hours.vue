5ccffe17b220ee04cfc43cf436e0e5e780b9887b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-27 17:12:59 -0500

    Northpark 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -37,6 +37,12 @@
                         <!-- Add Restaurant Drop Down -->
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i> {{}}</p>
+                        
+                    </div>
+                </div>
             </div>
             <div class="col-md-4">
                 <p>Find Us</p>
