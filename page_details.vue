94731e1ebb4f68bedfc7d06893fc9eab9191f13f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-27 15:51:05 -0500

    Northpark 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -13,58 +13,48 @@
 </template>
 
 <script>
-  define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
-    return Vue.component("page-details-component", {
-      template: template, // the variable template will be injected,
-      data: function() {
-        return {
-          success_subscribe : false,
-          currentPage: null
-        }
-      },
-      beforeRouteEnter (to, from, next) {
-        next(vm => {
-          // access to component instance via `vm`
-          
-            vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
-                
-                vm.currentPage = response.data;
-                console.log(vm.currentPage);
-            }, error => {
-                console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                vm.$router.replace({ name: '404'});
-            });
-            // vm.currentEvent = vm.findEventBySlug(to.params.id);
-            // if (vm.currentEvent === null || vm.currentEvent === undefined){
-                
-            // }
-        })
-      },
-      beforeRouteUpdate (to, from, next) {
-            this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
-                // this.dataLoaded = true;
-                this.currentPage = response.data;
-                console.log(this.currentPage);
-            }, error => {
-                console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                this.$router.replace({ name: '404'});
-            });
-        // this.currentEvent = this.findEventBySlug(to.params.id);
-        // if (this.currentEvent === null || this.currentEvent === undefined){
-        //   this.$router.replace({ name: '404'});
-        // }
-      },
-      computed: {
-        findEventBySlug () {
-          return this.$store.getters.findEventBySlug;
-        },
-        timezone() {
-          return this.$store.getters.getTimezone;
-        },
-        property (){
-            return this.$store.getters.getProperty;
-        }
-      }
+    define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
+        return Vue.component("page-details-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    success_subscribe : false,
+                    currentPage: null
+                }
+            },
+            beforeRouteEnter (to, from, next) {
+                next(vm => {
+                    // access to component instance via `vm`
+                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
+                        vm.currentPage = response.data;
+                        console.log(vm.currentPage);
+                    }, error => {
+                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                        vm.$router.replace({ name: '404'});
+                    });
+                })
+            },
+            beforeRouteUpdate (to, from, next) {
+                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
+                    // this.dataLoaded = true;
+                    this.currentPage = response.data;
+                    console.log(this.currentPage);
+                }, error => {
+                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                    this.$router.replace({ name: '404'});
+                });
+            },
+            computed: {
+                findEventBySlug () {
+                  return this.$store.getters.findEventBySlug;
+                },
+                timezone() {
+                  return this.$store.getters.getTimezone;
+                },
+                property (){
+                    return this.$store.getters.getProperty;
+                }
+            }
+        });
     });
-  });
 </script>
