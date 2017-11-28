1d10aa9c3cd0c0415e909221ccbd8d878afe3a43
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-28 16:23:40 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -19,7 +19,7 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-                <p class="page-breadcrumb">{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i> Hours</p>
+                <p class="page-breadcrumb">{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i></p>
             </div>
         </div>
     </div>
