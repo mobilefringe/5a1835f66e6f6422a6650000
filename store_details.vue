45a0a4ddf21c7ec57ec5bb1ca95cf316f48744df
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-28 16:54:49 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -35,6 +35,8 @@
             </div>
         </div>
         <div class="row is-table-row padding_tb_50" v-if="currentStore && currentStore.total_published_promos > 0">
+            <h5>Current Promotions</h5>
+            <hr>
             <div class="col-md-6" style="vertical-align:top;" v-for="promo in promotions" v-if="promotions">
                 <div class="col-md-6"> <img :src="promo.promo_image_url_abs" class="store_logo" alt=""> </div>
                 <div class="col-md-6">
