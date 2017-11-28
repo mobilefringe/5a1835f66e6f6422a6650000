3fb12adce0c39118a132b8ea2989f063d97e1116
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-28 12:00:45 -0500

    Northpark 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -82,8 +82,8 @@
                 restaurant(){
                     var stores_by_category = this.$store.getters.storesByCategoryName;
                     console.log(stores_by_category)
-                    // var cafes = stores_by_category.["NorthPark Cafés"]
-                    // console.log(cafes)
+                    var cafes = stores_by_category.[9]
+                    console.log(cafes)
                     var restaurants = _.filter(stores_by_category, function(o) { return o[i] = "NorthPark Cafés"})
                     console.log(restaurants)
                     
