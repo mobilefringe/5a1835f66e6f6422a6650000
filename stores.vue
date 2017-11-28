546bdc87cedf6294dc6100014c812c4f06130af1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-28 15:43:16 -0500

    Northpark 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -7,7 +7,7 @@
                 </div>
                 <div class="col-md-4">
                     <h2 class="dine-name">{{restaurant.name}}</h2>
-                    <router-link :to="{ name: 'storeDetails', params: { id: restaurant.slug }}">Visit Store Page <i class="fa fa-angle-double-right" aria-hidden="true"></i></router-link>
+                    <router-link :to="{ name: 'storeDetails', params: { id: restaurant.slug }}" classs="dine-link">Visit Store Page <i class="fa fa-angle-double-right" aria-hidden="true"></i></router-link>
                     <h5>Contact</h5>
                     <p>{{restaurant.phone}}</p>
                     <h5>Hours</h5>
