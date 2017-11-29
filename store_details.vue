36492dcc8912b3afe026ab6f6bfc441c1ea42d46
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-29 14:13:18 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -43,7 +43,7 @@
                 <div class="col-md-1">
                     <!--<p class=""> {{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}} </p>-->
                     <p class="details-promo-date">{{promo.start_date | moment("ddd", timezone)}}</p>
-                    <p>{{promo.start_date | moment("D", timezone)}}</p>
+                    <p class="details-promo-day">{{promo.start_date | moment("D", timezone)}}</p>
                     <p class="details-promo-date">{{promo.start_date | moment("MMM", timezone)}}</p>
                 </div>
                 <div class="col-md-5">
