b11e46f65eea2e7e4a312b5e86bde08a37e96a96
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-29 15:38:40 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="row">
-            <div class="col-md-6" v-for="event in events">
+            <div class="col-md-12">
+                <!-- SLIDER -->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row" v-for="event in events">
+                    <div class="col-md-4">
+                        
+                    </div>
+                    <div class="col-md-8">
+                        <router-link :to="{ name: 'eventDetails', params: { id: event.slug }}">
+                            <a class="details-link">Learn More <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                        </router-link>
+                    </div>
+                </div>
+            
                 <div class="card">
                     <div class="card-divider">
                       {{ event.name }}
