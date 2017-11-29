547d3de14bcb42f497c1d2c4a077c009069242cb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-29 10:13:51 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 require.config({
-  paths: {
-    'Vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.4.1/vue',
-    'vue_router': 'https://cdnjs.cloudflare.com/ajax/libs/vue-router/2.7.0/vue-router.min',
-    'axios': 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.1/axios.min',
-    'jquery': 'https://code.jquery.com/jquery-3.2.1.min',
-    'lodash': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min',
-    'moment': 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min',
-    'moment-timezone': 'https://momentjs.com/downloads/moment-timezone-with-data-2012-2022.min',
-    'vue2-filters': 'https://cdn.jsdelivr.net/vue2-filters/0.1.8/vue2-filters.min',
-    'vue': 'https://mmvue.codecloudapp.com/require-vuejs.min',
-    'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/2.3.1/vuex.min',
-    'vue-i18n': 'https://cdnjs.cloudflare.com/ajax/libs/vue-i18n/6.1.1/vue-i18n.min',
-    'text': 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
-    'json': 'https://unpkg.com/requirejs-plugins-current@1.0.3/src/json',
-    'js-cookie': 'https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.4/js.cookie.min',
-    'vue-meta': 'https://unpkg.com/vue-meta@1.0.4/lib/vue-meta.min',
-    'Fuse': 'https://cdnjs.cloudflare.com/ajax/libs/fuse.js/3.0.4/fuse.min',
-    'store': 'https://mmvue.codecloudapp.com/store',
-    'vue-moment': 'https://mmvue.codecloudapp.com/vue-moment',
-    'today_hours': 'https://mmvue.codecloudapp.com/today_hours.vue?noext', //append a dummy query string so requireJS doesn't auto-append .js to the end of the url
-    'search-component': 'https://mmvue.codecloudapp.com/search-component.vue?noext',
-    'vue-breadcrumb': 'https://cdn.jsdelivr.net/npm/vue-breadcrumbs@1.1.2/dist/vue-breadcrumbs.common.min'
-  }
+    paths: {
+        'Vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.4.1/vue',
+        'vue_router': 'https://cdnjs.cloudflare.com/ajax/libs/vue-router/2.7.0/vue-router.min',
+        'axios': 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.1/axios.min',
+        'jquery': 'https://code.jquery.com/jquery-3.2.1.min',
+        'lodash': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min',
+        'moment': 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min',
+        'moment-timezone': 'https://momentjs.com/downloads/moment-timezone-with-data-2012-2022.min',
+        'vue2-filters': 'https://cdn.jsdelivr.net/vue2-filters/0.1.8/vue2-filters.min',
+        'vue': 'https://mmvue.codecloudapp.com/require-vuejs.min',
+        'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/2.3.1/vuex.min',
+        'vue-i18n': 'https://cdnjs.cloudflare.com/ajax/libs/vue-i18n/6.1.1/vue-i18n.min',
+        'text': 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
+        'json': 'https://unpkg.com/requirejs-plugins-current@1.0.3/src/json',
+        'js-cookie': 'https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.4/js.cookie.min',
+        'vue-meta': 'https://unpkg.com/vue-meta@1.0.4/lib/vue-meta.min',
+        'Fuse': 'https://cdnjs.cloudflare.com/ajax/libs/fuse.js/3.0.4/fuse.min',
+        'store': 'https://mmvue.codecloudapp.com/store',
+        'vue-moment': 'https://mmvue.codecloudapp.com/vue-moment',
+        //append a dummy query string so requireJS doesn't auto-append .js to the end of the url
+        'today_hours': 'https://mmvue.codecloudapp.com/today_hours.vue?noext', 
+        'search-component': 'https://mmvue.codecloudapp.com/search-component.vue?noext',
+        'vue-breadcrumb': 'https://cdn.jsdelivr.net/npm/vue-breadcrumbs@1.1.2/dist/vue-breadcrumbs.common.min',
+    }
 });
 
 require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'locales', 'moment', 'vue-meta', 'vue-breadcrumb'], function (Vue, Vue2Filters, VueRouter, appRoutes, store, VueI18n, messages, moment, Meta, breadcrumb) {
