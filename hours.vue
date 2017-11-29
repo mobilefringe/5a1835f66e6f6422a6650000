079011d9070691f57a9a616498aa59f69acc1e5f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-29 11:58:19 -0500

    Northpark 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -83,7 +83,7 @@
                 next(vm => {
                     // access to component instance via `vm`
                     vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/northpark-management-hours" + ".json"}).then(response => {
-                        console.log(response.data)
+                        
                         vm.currentPage = response.data;
                         console.log(vm.currentPage);
                     }, error => {
@@ -92,15 +92,15 @@
                     });
                 })
             },
-            beforeRouteUpdate (to, from, next) {
-                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-management-hours.json"}).then(response => {
-                    this.currentPage = response.data;
-                    console.log(this.currentPage);
-                }, error => {
-                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                    this.$router.replace({ name: '404'});
-                });
-            },
+            // beforeRouteUpdate (to, from, next) {
+            //     this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-management-hours.json"}).then(response => {
+            //         this.currentPage = response.data;
+            //         console.log(this.currentPage);
+            //     }, error => {
+            //         console.error("Could not retrieve data from server. Please check internet connection and try again.");
+            //         this.$router.replace({ name: '404'});
+            //     });
+            // },
             computed: {
                 property(){
                     return this.$store.getters.getProperty;
