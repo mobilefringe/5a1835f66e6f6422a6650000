e27ff3788d876be2a9c192d9a2bd0c06dd45c262
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-29 16:04:17 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-md-12">
                 <!-- SLIDER -->
+                <img src="http://via.placeholder.com/960x460"/>
             </div>
         </div>
         <div class="row">
