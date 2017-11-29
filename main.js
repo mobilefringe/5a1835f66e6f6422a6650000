e2bc64c314b78b832392efce043b88586465d83d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-29 10:09:15 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -29,7 +29,8 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     Vue.use(VueRouter);
     Vue.use(Vue2Filters);
     Vue.use(VueI18n);
-
+    Vue.use(breadcrumb);
+    
     /* initialize router */
     const router = new VueRouter({
         mode: 'history',
