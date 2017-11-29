c9627cd022b1920f5b6a3d1aff76baa9889319c7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-29 15:02:07 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -6,7 +6,7 @@
                     <h2 class="details-store-name">{{currentStore.name}}</h2>
                     <p class="details-description">{{currentStore.description}}</p>
                     <router-link to="/stores" active-class="active" exact>
-                        <a class="details-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                        <a class="details-link details-map-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
                     </router-link>
                 </div>
             </div>
