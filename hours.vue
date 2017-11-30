fb4be6e7b37656234136d107c81ea5f13feb3240
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-30 16:01:46 -0500

    Northpark 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -63,7 +63,7 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-                <p class="page-breadcrumb">{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i><breadcrumbs>{{ breadcrumb }}</breadcrumbs></p>
+                <p class="page-breadcrumb">{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs></p>
             </div>
         </div>
     </div>
