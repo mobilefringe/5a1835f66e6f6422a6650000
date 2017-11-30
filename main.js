b72fcf29b185a25dba3c3613365882ac3640c4dd
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-30 12:51:47 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -58,7 +58,8 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             }
         },
         created() {
-            // make an async call to the data store to initialize the locale (i.e. it will check if there is a locale value saved in cookie, otherwise it will default to EN)
+            // make an async call to the data store to initialize the locale 
+            //(i.e. it will check if there is a locale value saved in cookie, otherwise it will default to EN)
             this.$store.dispatch('INITIALIZE_LOCALE');
           
             this.$store.dispatch('LOAD_META_DATA');
