9f9d07d0dc360f337550864d13c8a17418ef500d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-01 15:16:11 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -98,7 +98,7 @@
                     _.forEach(events, function(val) {
                         var tag_string = _.join(val.tags);
                         console.log(tag_string)
-                        if(tag_string == "holiday") {
+                        if(tag_string.indexOf("holiday") !== -1) {
                             console.log("YES!")
                             holiday_events.push(val);
                             console.log(holiday_events)
