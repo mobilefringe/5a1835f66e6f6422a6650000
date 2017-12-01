baf0417e34e60e2e3935d3121bef0540527a2d85
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-01 15:48:49 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -78,6 +78,7 @@
             computed: {
                 events() {
                     var events = this.$store.getters.processedEvents;
+                    console.log(events)
                     var promotions = this.$store.getters.processedPromos;
                     var merge = _.concat(events, promotions);
                     var sorted = _.orderBy(merge, function(o) { return o.start_date })
@@ -92,7 +93,7 @@
                     return promotions
                 },
                 holidayEvents(){
-                    var holiday_events = _.filter(this.$store.getters.processedEvents, function(o) { return o.tags.indexOf("holiday") });
+                    var events = _.filter(this.$store.getters.processedEvents, function(o) { return o.tags.length > 0 });
                     return holiday_events
                     console.log(holiday_events)
                    
