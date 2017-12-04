0093cc39e109bb5bcd039668045abbeac5c3b215
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-04 15:49:18 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -4,37 +4,20 @@
         <div class="row" v-if="currentBlog">
             <div class="col-md-9">
                 <div class="row">
-                    <div class="col-md-1">
+                    <div class="col-md-2">
                         <p class="details-promo-date">{{currentBlog.publish_date | moment("ddd", timezone)}}</p>
                         <p class="details-promo-day">{{currentBlog.publish_date | moment("D", timezone)}}</p>
                         <p class="details-promo-date">{{currentBlog.publish_date | moment("MMM", timezone)}}</p>
                     </div>
-                    <div class="col-md-7">
+                    <div class="col-md-10">
                         <p class="blog-category">Blog Category</p>
                         <h2 class="">{{currentBlog.title}}</h2> 
                         <p class="blog-author">By {{currentBlog.author}} | {{currentBlog.publish_date | moment("MM-D-YYYY", timezone)}}</p>
                         <img :src="currentBlog.image_url" class="margin_60" alt="" />
-                        <div>{{{currentBlog.html_body}}}</div>
-                    </div>
-                    <div class="col-md-4">
-                        
+                        <div>{{currentBlog.html_body}}</div>
                     </div>
                 </div>
-                
-            
-                
-                
-                
             </div>
-            <!--<div class="col-md-9">-->
-            <!--    <div class="details-store-info">-->
-            <!--        <h2 class="details-store-name">{{currentStore.name}}</h2>-->
-            <!--        <p class="details-description">{{currentStore.description}}</p>-->
-            <!--        <router-link to="/stores" active-class="active" exact>-->
-            <!--            <a class="details-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>-->
-            <!--        </router-link>-->
-            <!--    </div>-->
-            <!--</div>-->
             <div class="col-md-3">
                 <h5>Related Blogs</h5>
                 
