2ce42494f05b0d8d45607297cf09ea15fcfaf37d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-04 15:30:17 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="margin_90"></div>
-        <div class="row" v-if="currentPost">
-            <div class="col-md-12">
-                <img :src="currentPost.image_url" class="margin_60" alt="" />
-            </div>
+        <div class="row" v-if="currentBlog">
+            <!--<div class="col-md-12">-->
+            <!--    <img :src="currentStore.store_front_url_abs" class="margin_60" alt="" />-->
+            <!--</div>-->
             <!--<div class="col-md-9">-->
             <!--    <div class="details-store-info">-->
             <!--        <h2 class="details-store-name">{{currentStore.name}}</h2>-->
@@ -21,7 +21,7 @@
     
         <div class="row">
             <div class="col-md-12">
-                <p class="page-breadcrumb">{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i> News <i class="fa fa-angle-right" aria-hidden="true"></i> {{currentBlog.title}}</p>
+                <p class="page-breadcrumb">{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i> News <i class="fa fa-angle-right" aria-hidden="true"></i> {{currentBlog}}</p>
             </div>
         </div>
     </div>
@@ -36,32 +36,45 @@
                     title: "MM with Vue.js!",
                     description: "An example of integration of Mall Maverick with Vue.js",
                     blogName: "main",
-                    // currentBlog: null,
+                    currentBlog: null,
                     currentPost: null
                 }
             },
             mounted () {
-                this.currentPost = this.blogs("main");
+                this.currentBlog = this.blogs("main");
             },
             beforeRouteEnter(to, from, next) {
                 next(vm => {
                     // access to component instance via `vm`
                     var blogName = "main";
-                    vm.currentPost = vm.findBlogPostBySlug(blogName, to.params.id);
-                    console.log(vm.currentPost)
-                    if (vm.currentPost === null || vm.currentPost === undefined){
+                    vm.currentBlog = vm.findBlogPostBySlug(blogName, to.params.id);
+                    console.log(vm.currentBlog)
+                    if (vm.currentBlog === null || vm.currentBlog === undefined){
                         vm.$router.replace({ name: '404'});
                     }
                 })
             },
             beforeRouteUpdate(to, from, next) {
                 var blogName = "main";
-                this.currentPost = this.findBlogPostBySlug(blogName, to.params.id);
-                console.log(this.currentPost)
-                if (this.currentPost === null || this.currentPost === undefined){
+                this.currentBlog = this.findBlogPostBySlug(blogName, to.params.id);
+                console.log(this.currentBlog)
+                if (this.currentBlog === null || this.currentBlog === undefined){
                     this.$router.replace({ name: '404'});
                 }
             },
+            // watch: {
+            //     currentStore: function() {
+            //         console.log("currentStore promo", this.currentStore );
+            //         var vm = this;
+            //         var temp = [];
+            //         _.forEach(this.currentStore.promotions, function(value, key) {
+            //             console.log(vm.findPromoById(value));
+            //             temp.push(vm.findPromoById(value));
+            //         });
+            //         this.promotions = temp;
+            //         console.log("promos", this.promotions);
+            //     }
+            // },
             computed: {
                 findBlogPostBySlug () {
                     return this.$store.getters.findBlogPostBySlug;
