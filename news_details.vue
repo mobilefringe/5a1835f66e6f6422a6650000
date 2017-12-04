7f885a52777a11adfc04c284c2dde9dc80dc33e2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-04 16:13:35 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -16,7 +16,7 @@
                         <img :src="currentBlog.image_url" class="margin_60" alt="" />
                         <div>{{currentBlog.body}}</div>
                         
-                        <social-sharing url="https://vuejs.org/" title="The Progressive JavaScript Framework" description="Intuitive, Fast and Composable MVVM for building interactive interfaces." quote="Vue is a progressive framework for building user interfaces." hashtags="vuejs,javascript,framework" twitter-user="vuejs" inline-template>
+                        <social-sharing url="https://vuejs.org/" :title="currentBlog.title" :description="currentBlog.body" quote="Vue is a progressive framework for building user interfaces." hashtags="vuejs,javascript,framework" twitter-user="vuejs" inline-template>
                             <div>
                                 <network network="facebook">
                                     <i class="fa fa-facebook"></i> Facebook
