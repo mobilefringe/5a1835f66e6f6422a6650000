1b7cfca17af02211dab7c7edd701f72c6a6fabed
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-04 16:49:57 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -27,7 +27,7 @@
                         <p class="blog-category">Blog Category</p>
                         <h2 class="">{{currentBlog.title}}</h2> 
                         <p class="blog-author">By {{currentBlog.author}} | {{currentBlog.publish_date | moment("MM-D-YYYY", timezone)}}</p>
-                        <img :src="currentBlog.image_url" class="margin_60" alt="" />
+                        <!--<img :src="currentBlog.image_url" class="margin_60" alt="" />-->
                         <div v-html="currentBlog.html_body"></div>
                     </div>
                 </div>
