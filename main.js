dd87de7de668f9000dca43e57fdd21efed83605e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-04 14:15:43 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
         },
         mounted () {
             this.$store.state.results
-            console.log(this.$store.state.results.blogs)
+            console.log(this.$store.state)
         },
         created() {
             // make an async call to the data store to initialize the locale 
