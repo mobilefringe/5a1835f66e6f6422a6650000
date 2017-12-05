dfaccd831b96d892882e9fb17a5f7e91690b9eba
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-05 14:53:30 -0500

    Northpark 1.0  /news.vue

--- news.vue
+++ news.vue
@@ -122,7 +122,7 @@
                     console.log(this.$store.getters.findBlogByName)
                 },
                 blogBeauty() {
-                    // console.log(this.currentBlog)
+                    console.log(this.currentBlog)
                     // var beauty_blog = _.filter(this.currentBlog.posts, function(o) { return o.tags.join() });
                     // // return beauty_blog
                     // console.log(beauty_blog)
