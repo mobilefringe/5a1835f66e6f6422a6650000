8b837d8da07cb32dd221c901e3ce6e9677ffb59a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-06 16:56:38 -0500

    Northpark 1.0  /news.vue

--- news.vue
+++ news.vue
@@ -7,7 +7,7 @@
                     
                 </div>
                 <div class="col-md-6">
-                    <v-select v-model="selected" :options="categoryOptions" :searchable="false" class="open" :on-change="selectCategory()" label="name"></v-select>
+                    <v-select :options="categoryOptions"></v-select>
                     <!--<select class="" v-model="selected">-->
                     <!--    <option selected value="all_categories">All Events</option>-->
                     <!--    <option value="blogBeauty">Beauty</option>-->
