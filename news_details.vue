270776763234f9583f18851f31112e2c5516f9d9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-06 11:33:29 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -43,6 +43,15 @@
                         <button class="news-submit" type="submit"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
                     </form>
                 </div>
+                
+                <vue-instagram token="accessTokenHere" username="kevinongko" :count="5" :tags="['hashtag1', 'hashtag2']">
+    <template slot="feeds" slot-scope="props">
+      <li class="fancy-list"> {{ props.feed.link }} </li>
+    </template>
+    <template slot="error" slot-scope="props">
+      <div class="fancy-alert"> {{ props.error.error_message }} </div>
+    </template>
+  </vue-instagram>
             </div>
         </div>
     
