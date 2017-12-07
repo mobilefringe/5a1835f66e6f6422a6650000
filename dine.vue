3ec5e57dfe67ac31f8d4dedb00716122f0bfe33d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-07 15:23:49 -0500

    Northpark 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -20,16 +20,16 @@
                     <h5 class="dine-contact-header">Contact</h5>
                     <p>{{restaurant.phone}}</p>
                     <h5>Hours</h5>
-                    <ul class="">
-                        <li v-for="hour in storeHours({{restaurant.store_hours}})">
-                            <span class="pull-left col-md-6"> 
-                               {{day_of_the_week(hour.day_of_week)}}
-                            </span>
-                            <span class="pull-right col-md-6 text-left">
-                                {{hour.open_time | moment("h:mma", timezone)}} - {{hour.close_time | moment("h:mma", timezone)}}
-                            </span>
-                        </li>
-                    </ul>
+                    <!--<ul class="">-->
+                    <!--    <li v-for="hour in storeHours({{restaurant.store_hours}})">-->
+                    <!--        <span class="pull-left col-md-6"> -->
+                    <!--           {{day_of_the_week(hour.day_of_week)}}-->
+                    <!--        </span>-->
+                    <!--        <span class="pull-right col-md-6 text-left">-->
+                    <!--            {{hour.open_time | moment("h:mma", timezone)}} - {{hour.close_time | moment("h:mma", timezone)}}-->
+                    <!--        </span>-->
+                    <!--    </li>-->
+                    <!--</ul>-->
                 </div>
             </div>
         </div>
