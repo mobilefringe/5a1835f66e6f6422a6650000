55537815a5f270c8a2763f57a2cd8c3019d5e5f6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-07 14:44:56 -0500

    Northpark 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -27,9 +27,6 @@ define([], function () {
                 {
                     path: ':id',
                     component: view('store_details'),
-                    meta: {
-                        breadcrumb: 'Shops & Dining',
-                    },
                     name: 'storeDetails'
                 }
             ]
