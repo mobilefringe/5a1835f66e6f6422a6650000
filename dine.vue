760dc4b7509138cb43c8dd27db1d3443d7f33c06
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-07 15:33:12 -0500

    Northpark 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -21,7 +21,7 @@
                     <p>{{restaurant.phone}}</p>
                     <h5>Hours</h5>
                     <ul class="">
-                        <li v-for="hour in {{storeHours(restaurant.store_hours)}}">
+                        <li v-for="hour in storeHours(restaurant.store_hours)">
                             <span class="pull-left col-md-6"> 
                                {{day_of_the_week(hour.day_of_week)}}
                             </span>
