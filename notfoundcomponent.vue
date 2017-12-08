de16ece2226936253a4c606d690258a7be8bd3fb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-08 14:35:22 -0500

    Northpark 1.0  /notfoundcomponent.vue

--- notfoundcomponent.vue
+++ notfoundcomponent.vue
@@ -1,8 +1,36 @@
 <template>
     <div class="page-container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-8">
                 <h1>{{text}}</h1>
+            </div>
+            <div class="col-md-4">
+                <div class="sidebar">
+                    <div class="sidebar-container">
+                        <h5>Hours</h5>
+                        <ul class="sidebar-hours-list">
+                            <li v-for="hour in hours">
+                               {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
+                            </li>
+                        </ul> 
+                        <router-link to="/hours" active-class="active" exact>
+                            <a class="details-link">View Detailed Hours <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                        </router-link>
+                    </div>
+                    <div class="sidebar-container">
+                        <h5 class="">Find Us</h5>
+                        <p>
+                            {{property.name}}<br/>
+                            {{property.address1}}<br/>
+                            {{property.city}}, {{property.province_state}} {{property.postal_code}}<br/>
+                            {{property.contact_phone}}
+                        </p>
+                        <p>Northpark Center Concierge</p>
+                        <router-link to="" active-class="active" exact>
+                            <a class="details-link">Get Directions <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                        </router-link>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
