c576d3ccfacb1102e4a5d0ea2486183f75af1efa
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-08 11:26:28 -0500

    Northpark 1.0  /visit.vue

--- visit.vue
+++ visit.vue
@@ -123,7 +123,7 @@
                         vm.$router.replace({ name: '404'});
                     });     
                     //Tourism
-                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/northpark-international-visitors.json"}).then(response => {
+                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/northpark-tourism.json"}).then(response => {
                         vm.tourism = response.data;
                     }, error => {
                         console.error("Could not retrieve data from server. Please check internet connection and try again.");
@@ -161,7 +161,7 @@
                     this.$router.replace({ name: '404'});
                 });
                 //Tourism
-                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-international-visitors.json"}).then(response => {
+                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-tourism.json"}).then(response => {
                     this.giftCards = response.data;
                 }, error => {
                     console.error("Could not retrieve data from server. Please check internet connection and try again.");
