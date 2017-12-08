6b1853eb6b86e001d6610b845f3cdacee4c209a4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-08 14:19:00 -0500

    Northpark 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div class="margin-90"></div>
         <div class="row">
             <div class="col-md-8">
                 <h2>Shopping Centre Hours</h2>
