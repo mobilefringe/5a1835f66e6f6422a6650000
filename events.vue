8710bb5e46ad27e2c6339d881f027a873c607f68
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-11 15:57:25 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -47,7 +47,6 @@
 <script>
     define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta", "v-select", "vue-datepicker-local"], function(Vue, moment, tz, VueMoment, Meta, vSelect, VueDatepickerLocal) {
         Vue.use(Meta);
-        Vue.use(VueDatepickerLocal);
         
         return Vue.component("events-component", {
             template: template, // the variable template will be injected
