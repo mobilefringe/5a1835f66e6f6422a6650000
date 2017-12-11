640907bbc48f189a71501ef07291b75f316dfc44
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-11 12:54:28 -0500

    Northpark 1.0  /magazine.vue

--- magazine.vue
+++ magazine.vue
@@ -11,7 +11,18 @@
                 <h2>The Magazine</h2>
             </div>
             <div class="col-md-4">
-                
+                <div class="sidebar">
+                    <div class="sidebar-container">
+                        <h5>Get Connected</h5>
+                        <p>Subscribe to our newsletter and receive insider information on events, exclusive sales, special offers and much more.</p>
+                        <div class="blog-newsletter-container">
+                            <form action="https://mobilefringe.createsend.com/t/d/s/jhithd/" method="post">
+                                <input class="" id="fieldEmail" name="cm-jhithd-jhithd" type="email" required placeholder="Enter Your Email"/>
+                                <button class="news-submit" type="submit"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
