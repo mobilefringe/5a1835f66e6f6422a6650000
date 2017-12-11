207d74ff6f53010599b013d2e6360ceb38f0b68e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-11 15:18:40 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     // Vue.component('vue-breadcrumbs', VueBreadcrumbs);
     Vue.component('vue-instagram', VueInstagram);
     Vue.component('v-select', VueSelect.VueSelect);
-    Vue.component('vue-datepicker-local', VueDatePickerLocal);
+    Vue.component('vue-datepicker-local', VueDatepickerLocal);
     Vue.component('v-viewer', Viewer)
     
     /* bootstrap app */
