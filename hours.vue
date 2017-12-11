c86c563fb8fb2b0ca04b0314bf58169531b82e34
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-11 10:49:52 -0500

    Northpark 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -96,15 +96,15 @@
                 return {
                     currentPage: null,
                     selected: "Select a Restaurant",
-                    categoryOptions: this.restaurantData(),
+                    // categoryOptions: this.restaurantData(),
                     storeName: "",
                     storeHoursById: [],    
                 }
             },
-            mounted () {
-                this.restaurant,
-                // this.findStoreById
-            },
+            // mounted () {
+            //     this.restaurant,
+            //     this.findStoreById
+            // },
             // watch: {
             //     selected: function() {
             //         var vm = this;
@@ -171,7 +171,6 @@
                     return weekday[val_day];
                 },
                 restaurantData(){
-                    console.log(this.restaurants)
                     var restaurantData = [];
                     _.forEach(this.restaurants, function(value, key) {
                         var label = value.name;
