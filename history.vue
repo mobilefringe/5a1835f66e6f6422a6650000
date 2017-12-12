4b5110fe68003ba5a7c60332130d0eb214319606
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 15:20:22 -0500

    Northpark 1.0  /history.vue

--- history.vue
+++ history.vue
@@ -67,11 +67,8 @@
                         centerMode: true,
                         dots: true,
                         infinite: true,
-                        speed: 300,
-
-  
                         slidesToShow: 1,
-                 
+                        speed: 300,
                         
                         prevArrow: '.prev',
                         nextArrow: '.next',
