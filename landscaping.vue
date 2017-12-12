578f1cf67f8bd4ed4889a3992beaafe01b2f0c1c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 12:38:51 -0500

    Northpark 1.0  /landscaping.vue

---
+++ landscaping.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div class="row">
+            <div class="col-md-8">
+                <h2>Landscaping</h2>
+            </div>
+            <div class="col-md-4">
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="page-breadcrumb">{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
+        Vue.use(Meta);
+        return Vue.component("landscaping-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    currentPage: null,
+                }
+            },
+            // beforeRouteEnter (to, from, next) {
+            //     next(vm => {
+            //         // access to component instance via `vm`
+            //         vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/.json"}).then(response => {
+            //             vm.currentPage = response.data;
+            //         }, error => {
+            //             console.error("Could not retrieve data from server. Please check internet connection and try again.");
+            //             vm.$router.replace({ name: '404'});
+            //         });
+            //     })
+            // },
+            // beforeRouteUpdate (to, from, next) {
+            //     this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + ".json"}).then(response => {
+            //         this.currentPage = response.data;
+            //         console.log(this.currentPage);
+            //     }, error => {
+            //         console.error("Could not retrieve data from server. Please check internet connection and try again.");
+            //         this.$router.replace({ name: '404'});
+            //     });
+            // },
+            computed: {
+                property(){
+                    return this.$store.getters.getProperty;
+                },
+                timezone() {
+                    return this.$store.getters.getTimezone;
+                },
+                hours(){
+                    var hours = _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 })
+                    return hours;
+                },
+            },
+            methods: {
+                day_of_the_week(val_day){
+                    weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+                    return weekday[val_day];
+                },
+            }
+        });
+    });
+</script>
