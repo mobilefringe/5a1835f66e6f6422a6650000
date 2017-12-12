4485a1a5ca112f2c4bc6fab106710c3daf7ac0cc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 13:46:25 -0500

    Northpark 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="page-container">
+    <div>  <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="home-banner-container" style="background-image: url(https://picsum.photos/1920/1400);">
             <h1>Home</h1>    
         </div>
