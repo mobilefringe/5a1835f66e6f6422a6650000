7461a62e0594e8ac49a9fbefc7c9fb0933b517b5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 14:23:47 -0500

    Northpark 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -24,9 +24,6 @@
                 },
                 processedStores() {
                     return this.$store.getters.processedStores;
-                },
-                findRepoByName(){
-                    
                 }
             },
             methods: {
