aa8a87c98e54c5d5e4b4c0109ea56ed9409f4d64
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 11:52:44 -0500

    Northpark 1.0  /events_details.vue

--- events_details.vue
+++ events_details.vue
@@ -127,7 +127,8 @@
                 },
                 checkEventDates(currentEvent){
                     var timezone = this.timezone
-                    var start_date = moment(this.currentEvent.start_date | "MM-DD-YYYY", timezone)
+                    var start_date = moment(this.currentEvent.start_date).tz(timezone)
+                    // moment(this.currentEvent.start_date | "MM-DD-YYYY", timezone)
                     var end_date = moment(this.currentEvent.end_dat | "MM-DD-YYYY", timezone)
                     console.log(start_date, end_date)
                     if(start_date === end_date){
