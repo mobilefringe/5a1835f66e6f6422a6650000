f614e98138b6779bef0069c818bbdf23fc055f2d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-11 22:48:51 -0500

    Northpark 1.0  /events_details.vue

--- events_details.vue
+++ events_details.vue
@@ -27,8 +27,8 @@
                     <div class="sidebar-container" v-if="currentEvent && currentEvent.store">
                         <h5>Store Hours</h5>
                         <ul class="details-hours-list">
-                            <li v-for="hour in store_hours">
-                               {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
+                            <li v-for="store_hour in store_hours">
+                               {{day_of_the_week(store_hour.day_of_week)}} - {{store_hour.open_time | moment("h A", timezone)}} - {{store_hour.close_time | moment("h A", timezone)}}
                                 </span>
                             </li>
                         </ul>
