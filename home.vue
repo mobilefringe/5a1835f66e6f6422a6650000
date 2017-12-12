31f0fedc19929f704d9f83f9b3f6be94be2b6553
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 14:22:39 -0500

    Northpark 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -7,30 +7,30 @@
 </template>
 
 <script>
-  define(["Vue", "vue!today_hours", "vue!search-component"], function(Vue, TodayHoursComponent, SearchComponent) {
-    return Vue.component("home-component", {
-      template: template, // the variable template will be injected
-      data: function() {
-        return {
-          title: "MM with Vue.js!",
-          description: "An example of integration of Mall Maverick with Vue.js",
-          suggestionAttribute: 'name',
-          search: ''
-        }
-      },
-      computed: {
-        property(){
-          return this.$store.getters.getProperty;
-        },
-        processedStores() {
-          return this.$store.getters.processedStores;
-        }
-      },
-      methods: {
-        onOptionSelect(option) {
-          console.log('Selected option:', option)
-        }
-      }
+    define(["Vue", "vue!today_hours", "vue!search-component"], function(Vue, TodayHoursComponent, SearchComponent) {
+        return Vue.component("home-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    title: "MM with Vue.js!",
+                    description: "An example of integration of Mall Maverick with Vue.js",
+                    suggestionAttribute: 'name',
+                    search: ''
+                }
+            },
+            computed: {
+                property(){
+                    return this.$store.getters.getProperty;
+                },
+                processedStores() {
+                    return this.$store.getters.processedStores;
+                }
+            },
+            methods: {
+                onOptionSelect(option) {
+                    console.log('Selected option:', option)
+                }
+            }
+        })
     })
-  })
 </script>
