ad52290ca7ba972a0ebbeae9318d84f17fd81de3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 11:10:07 -0500

    Northpark 1.0  /events_details.vue

--- events_details.vue
+++ events_details.vue
@@ -128,7 +128,7 @@
                     console.log(this.currentEvent)
                     if(this.currentEvent.start_date === this.currentEvent.end_date){
                         console.log("Yes!")
-                        var one_day_event = moment(this.currentEvent.start_date, "dddd, MMMM D YYYY", timezone)
+                        var one_day_event = this.currentEvent.start_date | moment("h A", timezone)
                         console.log(one_day_event)
                         // return 
                     } else {
