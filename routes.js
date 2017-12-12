c3e77b32e2825e958d1bb35099523cfcf067c824
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 12:32:53 -0500

    Northpark 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -178,7 +178,7 @@ define([], function () {
                     name: 'ourStory'
                 },
                 {
-                    path: '/history',
+                    path: '/our-story/history',
                     component: view('history'),
                     name: 'history'
                 },
