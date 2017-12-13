46c337ca57415778521100f8446f4eaf11a43f60
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-13 14:31:22 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -126,8 +126,8 @@
                     this.hours = storeHours;
                     
                     console.log(this.currentStore)
-                    if(this.currentstore.category_name != null) {
-                        console.log(this.currentstore.category_name)
+                    if(this.currentStore.category_name != null) {
+                        console.log(this.currentStore.category_name)
                         var category_name = this.currentstore.category_name
                         if(category_name == "NorthPark Cafés" || category_name == "Restaurants / Beverages" || category_name == "Specialty Foods"){
                             this.isDine = true;
