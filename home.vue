df5d3eb182f2028c29543b83b40a6e8054bb11a8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-13 11:50:16 -0500

    Northpark 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -21,61 +21,6 @@
     </div>
 </template>
 
-<style>
-    .no-overflow {
-  overflow: hidden;
-}
-.columns{
-  position:relative;
-}
-.hero-image {
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center center;
-  height: 550px;
-  position: relative;
-  width: 100%;
-}
-.hero-text {
-  font-size: 50px;
-  color: white;;
-   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  text-align: center;
-}
-.slick-slider { overflow: hidden; }
-.slick-slider div.slick-active .hero-image {
-  -webkit-animation: myMove 8s 1 ease-in-out;
-  -moz-animation: myMove 8s 1 ease-in-out;
-  -o-animation: myMove 8s 1 ease-in-out;
-  -ms-animation: myMove 8s 1 ease-in-out;
-  animation: myMove 8s 1 ease-in-out;
-}
-@keyframes myMove {
-  from { transform: scale(1.0,1.0); transform-origin: 50% 50%; }
-  to { transform: scale(1.1,1.1); transform-origin: 50% 0%; }
-}
-@-webkit-keyframes myMove {
-  from { -webkit-transform: scale(1.0,1.0); -webkit-transform-origin: 50% 50%; }
-  to { -webkit-transform: scale(1.1,1.1); -webkit-transform-origin: 50% 0%;  }
-}
-@-o-keyframes myMove {
-  from { -o-transform: scale(1.0,1.0); -o-transform-origin: 50% 50%; }
-  to { -o-transform: scale(1.1,1.1); -o-transform-origin: 50% 0%;  }
-}
-@-moz-keyframes myMove {
-  from { -moz-transform: scale(1.0,1.0); -moz-transform-origin: 50% 50%; }
-  to { -moz-transform: scale(1.1,1.1); -moz-transform-origin: 50% 0%; }
-}
-@-ms-keyframes myMove {
-  from { -ms-transform: scale(1.0,1.0); -ms-transform-origin: 50% 50%; }
-  to { -ms-transform: scale(1.1,1.1); -ms-transform-origin: 50% 0%; }
-}
-</style>
 <script>
     define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta", "vue!vue-slick"], function(Vue, moment, tz, VueMoment, Meta, slick) {
         Vue.use(Meta);
