ee13cae9501f4f89da0ffef55d72e27411d9c299
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-13 12:47:22 -0500

    Northpark 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -44,7 +44,7 @@
                         fade: true,
                         infinite: true,
                         slidesToShow: 1,
-                        speed: 1500,
+                        speed: 1600,
                         prevArrow: '.prev',
                         nextArrow: '.next'
                     }
