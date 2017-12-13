9ac4e6b71a84a18a0aeb1cdf228a136800bbb410
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-12 20:29:58 -0500

    Northpark 1.0  /landscaping.vue

--- landscaping.vue
+++ landscaping.vue
@@ -1,16 +1,87 @@
 <template>
-    <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <div class="row">
-            <div class="col-md-8">
-                <h2>Landscaping</h2>
+    <div class=""> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <img src="http://via.placeholder.com/1920x640" alt="" />
+        <div class="page-container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- QUOTE -->
+                    <h1 class="art-quote">NORTHPARK CENTER HAS A LONG AND GREAT TRADITION OF EXHIBITING SIGNIFICANT CONTEMPORARY ARTWORK, AND I THINK IT IS WONDERFUL THAT THE TRADITION CONTINUES INTO THE PRESENT.</h1>
+                    <p class="art-quote-subtext">JOEL SHAPIRO</p>
+                </div>
             </div>
-            <div class="col-md-4">
-                
+            <div class="row image-row">
+                <div class="col-md-4">
+                    <div class="hover-container">
+                        <img class="hover-image" src="http://via.placeholder.com/308x410" alt="" />
+                    	<div class="hover-text-container hover-scale">
+                    		<div class="hover-text">Lorem ipsum dolor sit amet</div>
+                    	</div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover-container">
+                        <img class="hover-image" src="http://via.placeholder.com/308x410" alt="" />
+                    	<div class="hover-text-container hover-scale">
+                    		<div class="hover-text">Lorem ipsum dolor sit amet</div>
+                    	</div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover-container">
+                        <img class="hover-image" src="http://via.placeholder.com/308x410" alt="" />
+                    	<div class="hover-text-container hover-scale">
+                    		<div class="hover-text">Lorem ipsum dolor sit amet</div>
+                    	</div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="page-breadcrumb">{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs></p>
+        <div class="image-row">
+            <div class="hover-container">
+                <img class="hover-image" src="http://via.placeholder.com/1920x640" alt="" />
+            	<div class="hover-text-container hover-scale">
+            		<div class="hover-text">Lorem ipsum dolor sit amet</div>
+            	</div>
+            </div>
+        </div>
+        <div class="page-container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="hours-management" v-html="currentPage.body"></div>
+                </div>
+            </div>
+            <div class="row image-row">
+                <div class="col-md-6">
+                    <div class="hover-container">
+                        <img class="hover-image" src="http://via.placeholder.com/468x410" alt="" />
+                    	<div class="hover-text-container hover-scale">
+                    		<div class="hover-text">Lorem ipsum dolor sit amet</div>
+                    	</div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="hover-container">
+                        <img class="hover-image" src="http://via.placeholder.com/468x410" alt="" />
+                    	<div class="hover-text-container hover-scale">
+                    		<div class="hover-text">Lorem ipsum dolor sit amet</div>
+                    	</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row image-row">
+                <div class="col-md-12">
+                    <div class="hover-container">
+                        <img class="hover-image" src="http://via.placeholder.com/960x530" alt="" />
+                    	<div class="hover-text-container hover-scale">
+                    		<div class="hover-text">Lorem ipsum dolor sit amet</div>
+                    	</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="page-breadcrumb">{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs></p>
+                </div>
             </div>
         </div>
     </div>
@@ -26,19 +97,19 @@
                     currentPage: null,
                 }
             },
-            // beforeRouteEnter (to, from, next) {
-            //     next(vm => {
-            //         // access to component instance via `vm`
-            //         vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/.json"}).then(response => {
-            //             vm.currentPage = response.data;
-            //         }, error => {
-            //             console.error("Could not retrieve data from server. Please check internet connection and try again.");
-            //             vm.$router.replace({ name: '404'});
-            //         });
-            //     })
-            // },
+            beforeRouteEnter (to, from, next) {
+                next(vm => {
+                    // access to component instance via `vm`
+                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/northpark-about-the-collection" + ".json"}).then(response => {
+                        vm.currentPage = response.data;
+                    }, error => {
+                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                        vm.$router.replace({ name: '404'});
+                    });
+                })
+            },
             // beforeRouteUpdate (to, from, next) {
-            //     this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + ".json"}).then(response => {
+            //     this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-management-hours.json"}).then(response => {
             //         this.currentPage = response.data;
             //         console.log(this.currentPage);
             //     }, error => {
@@ -57,12 +128,29 @@
                     var hours = _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 })
                     return hours;
                 },
+                restaurants(){
+                    var stores_by_category = this.$store.getters.storesByCategoryName;
+                    var cafes = stores_by_category["NorthPark Cafés"]
+                    var restaurants = stores_by_category["Restaurants / Beverages"]
+                    var specialty = stores_by_category["Specialty Foods"]
+                    var all_restaurants = _.concat(cafes, restaurants, specialty)
+                    var filtered_restaurants = _.uniqBy(all_restaurants, function(o){ return o.name; })
+                    return filtered_restaurants
+                }
             },
             methods: {
                 day_of_the_week(val_day){
                     weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
                     return weekday[val_day];
                 },
+                changeItem(event) {
+                    this.selected = `${event.target.value}`
+                    console.log("I've changed")
+                    console.log(this.selected)
+                    
+                    // var store_id = this.${event.target.value}
+                    console.log(store_id)
+                }
             }
         });
     });
