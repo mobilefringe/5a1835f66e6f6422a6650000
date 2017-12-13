6b2700792428f68de46e12a45cad74c47b4031db
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-13 14:25:50 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -74,7 +74,7 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-                <p class="page-breadcrumb">{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<span v-if=""><i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>{{currentStore.name }}</p>
+                <p class="page-breadcrumb">{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<span v-if=""><i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span><span v-if="currentStore">{{currentStore.name }}</span></p>
             </div>
         </div>
     </div>
