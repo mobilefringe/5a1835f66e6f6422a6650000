8ca0335cedfd75e41621a6151338bcba40eeccd4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 12:27:05 -0500

    Northpark 1.0  /new_stores.vue

--- new_stores.vue
+++ new_stores.vue
@@ -1,23 +1,47 @@
 <template>
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="margin-60"></div>
-        <div class="dine-container" v-for="store in newStoresList">
-            <div class="row">
-                <div class="col-md-9">
-                    <img :src="store.image_url" :alt="store.name" />
+        <div v-if="newStoresList">
+            <div class="dine-container" v-for="store in newStoresList">
+                <div class="row">
+                    <div class="col-md-9">
+                        <img :src="store.image_url" :alt="store.name" />
+                    </div>
+                    <div class="col-md-3">
+                        <h2 class="dine-name">{{store.name}}</h2>
+                        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}" class="dine-link">Visit Store Page <i class="fa fa-angle-double-right" aria-hidden="true"></i></router-link>
+                        <h5 class="dine-contact-header">Contact</h5>
+                        <p>{{store.phone}}</p>
+                        <h5>Hours</h5>
+                        <ul class="details-hours-list">
+                            <li v-for="hour in storeHours(store.store_hours)">
+                               {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <h2 class="dine-name">{{store.name}}</h2>
-                    <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}" class="dine-link">Visit Store Page <i class="fa fa-angle-double-right" aria-hidden="true"></i></router-link>
-                    <h5 class="dine-contact-header">Contact</h5>
-                    <p>{{store.phone}}</p>
-                    <h5>Hours</h5>
-                    <ul class="details-hours-list">
-                        <li v-for="hour in storeHours(store.store_hours)">
-                           {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
-                            </span>
-                        </li>
-                    </ul>
+            </div>
+        </div>
+        <div v-if="comingSoonList">
+            <div class="dine-container" v-for="store in newStoresList">
+                <div class="row">
+                    <div class="col-md-9">
+                        <img :src="store.image_url" :alt="store.name" />
+                    </div>
+                    <div class="col-md-3">
+                        <h2 class="dine-name">{{store.name}}</h2>
+                        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}" class="dine-link">Visit Store Page <i class="fa fa-angle-double-right" aria-hidden="true"></i></router-link>
+                        <h5 class="dine-contact-header">Contact</h5>
+                        <p>{{store.phone}}</p>
+                        <h5>Hours</h5>
+                        <ul class="details-hours-list">
+                            <li v-for="hour in storeHours(store.store_hours)">
+                               {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -47,6 +71,9 @@
                 },
                 newStoresList() {
                     return this.$store.getters.findNewStores;
+                },
+                comingSoonList() {
+                    return this.$store.getters.findComingSoonStores;
                 },
                 findHourById() {
                     return this.$store.getters.findHourById;
