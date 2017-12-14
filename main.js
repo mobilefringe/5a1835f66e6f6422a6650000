d958e686055bc43f5f4ecfda68bc6488eb416b8f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 12:21:40 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                 } else {
                     this.isHomePage = true;
                 }
-                if(this.$route.path == "/stores" || this.$route.path == "/dine" || this.$route.path == "/new-doors" || _.includes(this.$route.path,"/stores") {
+                if(this.$route.path == "/stores" || this.$route.path == "/dine" || this.$route.path == "/new-doors") {
                     this.showStoresMenu = true;
                 } else {
                     this.showStoresMenu = false;
