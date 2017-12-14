890d83047da3d7210b8529079df2ac71d5f38dcb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 13:44:44 -0500

    Northpark 1.0  /art_photo_gallery.vue

--- art_photo_gallery.vue
+++ art_photo_gallery.vue
@@ -64,7 +64,7 @@
                     return this.$store.getters.getTimezone;
                 },
                 pois() {
-                    return this.$store.getters.getPointsOfInterest;
+                    return this.$store.getters.getPointsOfInterest.locations;
                 },
                 sortByName() {
                     
