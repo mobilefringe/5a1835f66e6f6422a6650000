e3961a12f02005b972b74a53f4bbc5cef7ebc144
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 14:25:43 -0500

    Northpark 1.0  /art_photo_gallery.vue

--- art_photo_gallery.vue
+++ art_photo_gallery.vue
@@ -36,10 +36,9 @@
 </template>
 
 <script>
-    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta", "v-select", "vue-pure-lightbox"], function(Vue, moment, tz, VueMoment, Meta, vSelect, Lightbox) {
+    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta", "v-select"], function(Vue, moment, tz, VueMoment, Meta, vSelect) {
         Vue.use(Meta);
-        // Vue.use(Lightbox);
-        
+
         return Vue.component("art-photo-gallery-component", {
             template: template, // the variable template will be injected
             data: function() {
