e1ec4f93b61b99901967d158936ab7ba25714b8e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 11:52:45 -0500

    Northpark 1.0  /new_stores.vue

--- new_stores.vue
+++ new_stores.vue
@@ -5,13 +5,13 @@
         <div class="dine-container" v-for="store in newStores">
             <div class="row">
                 <div class="col-md-9">
-                    <img :src="restaurant.image_url" :alt="restaurant.name" />
+                    <img :src="store.image_url" :alt="store.name" />
                 </div>
                 <div class="col-md-3">
-                    <h2 class="dine-name">{{restaurant.name}}</h2>
-                    <router-link :to="{ name: 'storeDetails', params: { id: restaurant.slug }}" class="dine-link">Visit Store Page <i class="fa fa-angle-double-right" aria-hidden="true"></i></router-link>
+                    <h2 class="dine-name">{{store.name}}</h2>
+                    <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}" class="dine-link">Visit Store Page <i class="fa fa-angle-double-right" aria-hidden="true"></i></router-link>
                     <h5 class="dine-contact-header">Contact</h5>
-                    <p>{{restaurant.phone}}</p>
+                    <p>{{store.phone}}</p>
                     <h5>Hours</h5>
                     <ul class="details-hours-list">
                         <li v-for="hour in storeHours(restaurant.store_hours)">
