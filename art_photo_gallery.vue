2f9e05e2710ea6a7afc3a4955ac218abbfb33533
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 14:37:39 -0500

    Northpark 1.0  /art_photo_gallery.vue

--- art_photo_gallery.vue
+++ art_photo_gallery.vue
@@ -81,7 +81,6 @@
                         var items = value.items
                         if(items != null){
                             var images = items[0].images
-                            console.log(images)
                             if(images != null){
                                 var main_image = images[0].image_url
                                 console.log(main_image)
