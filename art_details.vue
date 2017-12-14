7b3a9007e130b9c97d1c4cb11194a9b14dcd03c1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 15:54:54 -0500

    Northpark 1.0  /art_details.vue

--- art_details.vue
+++ art_details.vue
@@ -1,25 +1,51 @@
-<template>
-    <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <div class="row" v-if="currentPoint">
-            <!--<div class="col-md-9">-->
-            <!--    <div class="details-store-info">-->
-            <!--        <h2 class="details-store-name">{{currentStore.name}}</h2>-->
-            <!--        <p class="details-description">{{currentStore.description}}</p>-->
-            <!--        <router-link to="/stores" active-class="active" exact>-->
-            <!--            <a class="details-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>-->
-            <!--        </router-link>-->
-            <!--    </div>-->
-            <!--</div>-->
-            <!--<div class="col-md-3">-->
-            <!--    <div class="details-contact-container" v-if="currentStore && currentStore.phone">-->
-            <!--        <h5 class="dine-contact-header">Contact</h5>-->
-            <!--        <p>{{currentStore.phone}}</p>-->
-            <!--    </div>-->
-            <!--</div>-->
+<div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div class="margin-90"></div>
+        <div class="row" v-if="currentStore">
+            <div class="col-md-12 margin-60">
+                <slick v-if="currentDetails" ref="slick" :options="slickOptions">
+                    <div v-for="asset in storeAssets" >
+                        <img :src="asset.url" alt="">
+                    </div>
+                </slick>
+                <img v-if="!currentStore.assets" :src="currentStore.store_front_url_abs" alt="" />
+            </div>
+            <div class="col-md-8">
+                <div class="details-store-info">
+                    <h2 class="details-store-name">{{currentStore.name}}</h2>
+                    <p class="details-description">{{currentStore.description}}</p>
+                    <div class="details-reservation" v-if="currentStore && currentStore.website">
+                        <div v-if="!isDine">
+                            <a class="details-link" :href="'http://' + currentStore.website" target="_blank">View Store Website&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                        </div>
+                        <div v-if="isDine">
+                            <h5>Reservations</h5>
+                            <a class="details-link" :href="'http://' + currentStore.website" target="_blank">Make Reservations&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                    <router-link to="/stores" active-class="active" exact>
+                        <a class="details-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                    </router-link>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="sidebar">
+                    <div class="sidebar-container" v-if="currentpoint">
+                        <h5></h5>
+                        <p>This content needs to be added</p>
+                    </div>
+                </div>
+                
+            </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <p class="page-breadcrumb">{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i></p>
+                <p class="page-breadcrumb">
+                    <span>{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
+                    <breadcrumbs>{{ breadcrumb }}</breadcrumbs>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;
+                    <span v-if="isNewStore"><router-link to="/new-doors">New Doors</router-link>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
+                    <span v-if="!isNewStore || isDine"><router-link to="/dine">Dine</router-link>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
+                    <span v-if="currentStore">{{currentStore.name }}</span>
+                </p>
             </div>
         </div>
     </div>
