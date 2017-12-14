2502db414269eb9eb291d664ac56d1a4db55efe3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 15:04:24 -0500

    Northpark 1.0  /art_details.vue

--- art_details.vue
+++ art_details.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <div class="row" v-if="currentStore">
-            <div class="col-md-9">
-                <div class="details-store-info">
-                    <h2 class="details-store-name">{{currentStore.name}}</h2>
-                    <p class="details-description">{{currentStore.description}}</p>
-                    <router-link to="/stores" active-class="active" exact>
-                        <a class="details-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
-                    </router-link>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="details-contact-container" v-if="currentStore && currentStore.phone">
-                    <h5 class="dine-contact-header">Contact</h5>
-                    <p>{{currentStore.phone}}</p>
-                </div>
-            </div>
+        <div class="row" v-if="currentItem">
+            <!--<div class="col-md-9">-->
+            <!--    <div class="details-store-info">-->
+            <!--        <h2 class="details-store-name">{{currentStore.name}}</h2>-->
+            <!--        <p class="details-description">{{currentStore.description}}</p>-->
+            <!--        <router-link to="/stores" active-class="active" exact>-->
+            <!--            <a class="details-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>-->
+            <!--        </router-link>-->
+            <!--    </div>-->
+            <!--</div>-->
+            <!--<div class="col-md-3">-->
+            <!--    <div class="details-contact-container" v-if="currentStore && currentStore.phone">-->
+            <!--        <h5 class="dine-contact-header">Contact</h5>-->
+            <!--        <p>{{currentStore.phone}}</p>-->
+            <!--    </div>-->
+            <!--</div>-->
         </div>
         <div class="row">
             <div class="col-md-12">
@@ -31,46 +31,40 @@
             template: template, // the variable template will be injected,
             data: function() {
                 return {
-                    title: "MM with Vue.js!",
-                    description: "An example of integration of Mall Maverick with Vue.js",
-                    currentStore: null,
-                    promotions : []
+                    currentItem: null,
                 }
             },
             beforeRouteEnter(to, from, next) {
                 next(vm => {
                     // access to component instance via `vm`
-                    vm.currentStore = vm.findStoreBySlug(to.params.id);
-                    if (vm.currentStore === null || vm.currentStore === undefined){
+                    vm.currentItem = vm.findStoreBySlug(to.params.id);
+                    if (vm.currentItem === null || vm.currentItem === undefined){
                         vm.$router.replace({ name: '404'});
                     }
                 })
             },
             beforeRouteUpdate(to, from, next) {
-                this.currentStore = this.findStoreBySlug(to.params.id);
-                if (this.currentStore === null || this.currentStore === undefined){
+                this.currentItem = this.findStoreBySlug(to.params.id);
+                if (this.currentItem === null || this.currentItem === undefined){
                     this.$router.replace({ name: '404'});
                 }
             },
             watch: {
-                currentStore: function() {
-                    console.log("currentStore promo", this.currentStore );
+                currentItem: function() {
+                    console.log(this.currentItem);
                     var vm = this;
-                    var temp = [];
-                    _.forEach(this.currentStore.promotions, function(value, key) {
-                        console.log(vm.findPromoById(value));
-                        temp.push(vm.findPromoById(value));
-                    });
-                    this.promotions = temp;
-                    console.log("promos", this.promotions);
+                    // var temp = [];
+                    // _.forEach(this.currentStore.promotions, function(value, key) {
+                    //     console.log(vm.findPromoById(value));
+                    //     temp.push(vm.findPromoById(value));
+                    // });
+                    // this.promotions = temp;
+                    // console.log("promos", this.promotions);
                 }
             },
             computed: {
                 findStoreBySlug () {
                     return this.$store.getters.findStoreBySlug;
-                },
-                findPromoById () {
-                    return this.$store.getters.findPromoById;
                 },
                 property(){
                     return this.$store.getters.getProperty;
@@ -80,10 +74,7 @@
                 },
             },
             methods: {
-                truncate(val_description){
-                    var truncate = _.truncate(val_description, { 'length': 249, 'separator': ' ' });
-                    return truncate;
-                },
+                
             }
         });
     });
