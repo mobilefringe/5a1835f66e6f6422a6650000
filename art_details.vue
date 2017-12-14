4c1340c9ed07dcdd3db2b4bead3023fc91aabdf1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 17:07:30 -0500

    Northpark 1.0  /art_details.vue

--- art_details.vue
+++ art_details.vue
@@ -13,6 +13,7 @@
                 <div class="details-store-info">
                     <h2 class="details-store-name">{{currentPoint.name}}</h2>
                     <p class="details-description">{{currentPoint.items[0].description}}</p>
+                    <p class="details-description">{{currentPoint.items[0].description_2}}</p>
                 </div>
             </div>
             <div class="col-md-4">
