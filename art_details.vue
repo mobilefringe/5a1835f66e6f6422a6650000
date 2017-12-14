eda89db60b9156f27ee52459a4553f564bcd3cfd
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-14 15:56:04 -0500

    Northpark 1.0  /art_details.vue

--- art_details.vue
+++ art_details.vue
@@ -51,7 +51,9 @@
 </template>
 
 <script>
-    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
+    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta", "vue!vue-slick"], function(Vue, moment, tz, VueMoment, Meta, slick) {
+        Vue.use(Meta);
+        
         return Vue.component("art-details-component", {
             template: template, // the variable template will be injected,
             data: function() {
