700db623aab5248d8982455c14f95c700d33842b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-15 13:11:26 -0500

    Northpark 1.0  /art.vue

--- art.vue
+++ art.vue
@@ -1,6 +1,6 @@
 <template>
     <div class=""> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <div v-for="item in sectionOne">
+        <div v-for="item in sectionOne" v-if="sectionOne">
             <img :src="item.photo_url" alt="" />    
             <div class="page-container">
                 <div class="row">
