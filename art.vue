1dc1f248e64598127244737bc7c889a801317180
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-15 10:43:52 -0500

    Northpark 1.0  /art.vue

--- art.vue
+++ art.vue
@@ -134,6 +134,7 @@
                 },
                 images() {
                     var repo = _.filter(this.$store.state.results.repos, function(o) { return o.name == "Art Collection Overview" })
+                    console.log(repo)
                     var repo_images = repo[0].images
                     console.log(repo_images)
                     return repo_images
