c1b2f6e0a6c318e80a24b6152de9f66c79b5e51d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-18 18:02:42 -0500

    Northpark 1.0  /holiday.vue

--- holiday.vue
+++ holiday.vue
@@ -18,7 +18,7 @@
                 </div>
                 <div class="col-md-12" v-if="sectionTwo">
                     <a :href="sectionTwo.image_url" target="_blank">
-                        <p class="art-quote-subtext">Download Holiday Map</p>
+                        <p class="holiday_download">Download Holiday Map</p>
                     </a>
                 </div>
             </div>
