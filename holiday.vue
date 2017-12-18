1cac59b9d9b3d014c2232238165cb954c55495ac
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-18 17:42:05 -0500

    Northpark 1.0  /holiday.vue

--- holiday.vue
+++ holiday.vue
@@ -46,7 +46,29 @@
                     </div>
                 </div>
                 <div class="row">
+                    <div class="col-md-7" v-if="currentSelection">
+                        <div class="row" v-for="event in currentSelection">
+                            <div class="col-md-2">
+                                <p class="details-promo-date">{{event.start_date | moment("ddd", timezone)}}</p>
+                                <p class="details-promo-day">{{event.start_date | moment("D", timezone)}}</p>
+                                <p class="details-promo-date">{{event.start_date | moment("MMM", timezone)}}</p>
+                            </div>
+                            <div class="col-md-10">
+                                <h5 class="details-dates">{{ checkEventDates(event) }}</h5>
+                                <h2>{{ event.name }}</h2>
+                                <p class="sub_title" v-if="event.store">{{ event.store.name }}</p><p class="sub_title" v-else>{{ property.name}}</p>
+                                <router-link :to="{ name: 'eventDetails', params: { id: event.slug }}">
+                                    <p class="details-link">Learn More <i class="fa fa-angle-double-right" aria-hidden="true"></i></p>
+                                </router-link>
+                            </div>
+                            <div class="col-md-12">
+                                <hr>    
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">    
                     
+                    </div>
                 </div>
             </div>
             
