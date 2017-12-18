f2604e5a68021a8cf27111ae358d46aefb5ced8d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-18 11:43:08 -0500

    Northpark 1.0  /our_story.vue

--- our_story.vue
+++ our_story.vue
@@ -1,24 +1,204 @@
 <template>
-    <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <div class="row">
-            <div class="col-md-8">
-                <h2>Our Story</h2>
-            </div>
-            <div class="col-md-4">
-                
+    <div class=""> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div v-if="sectionOne">
+            <div class="gallery-banner" v-bind:style="{ backgroundImage: 'url(' + sectionOne.image_url + ')' }"></div>
+            <div class="page-container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="art-quote">{{ sectionOne.description }}</h1>
+                        <p class="art-quote-subtext">-{{ sectionOne.name }}</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="page-breadcrumb">{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs></p>
+        <div class="page-container">
+            <div class="row image-row">
+                <div class="col-md-4" v-if="sectionTwo">
+                    <div class="hover-container">
+                        <div class="hover-image" v-bind:style="{ backgroundImage: 'url(' + sectionTwo.image_url + ')' }"></div>
+                    	<div v-if="sectionTwo.url" class="hover-text-container hover-scale">
+                		    <div class="hover-text">
+                		        <h2>{{ sectionTwo.name }}</h2>
+                		        <h4>{{ sectionTwo.description }}</h4>
+                		        <router-link :to="sectionTwo.url">
+                		            <h5>Learn More&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                		        </router-link>
+            		        </div>
+                    	</div>
+                    	<div v-if="!sectionTwo.url"class="hover-text-container hover-scale">
+                    		<div class="hover-text">
+                    		    <h2>{{ sectionTwo.name }}</h2>
+                    		    <h4>{{ sectionTwo.description }}</h4>
+                    		    <a :href="sectionTwo.image_url" :data-lightbox="sectionTwo.name">
+                    		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                    		    </a>
+                		    </div>
+                    	</div>
+                    </div>
+                </div>
+                <div class="col-md-4" v-if="sectionThree">
+                    <div class="hover-container">
+                        <div class="hover-image" v-bind:style="{ backgroundImage: 'url(' + sectionThree.image_url + ')' }"></div>
+                    	<div v-if="sectionThree.url" class="hover-text-container hover-scale">
+                		    <div class="hover-text">
+                		        <h2>{{ sectionThree.name }}</h2>
+                		        <h4>{{ sectionThree.description }}</h4>
+                		        <router-link :to="sectionThree.url">
+                		            <h5>Learn More&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                		        </router-link>
+            		        </div>
+                    	</div>
+                    	<div v-if="!sectionThree.url"class="hover-text-container hover-scale">
+                    		<div class="hover-text">
+                    		    <h2>{{ sectionThree.name }}</h2>
+                    		    <h4>{{ sectionThree.description }}</h4>
+                    		    <a :href="sectionThree.image_url" :data-lightbox="sectionThree.name">
+                    		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                		        </a>
+                		    </div>
+                    	</div>
+                    </div>
+                </div>
+                <div class="col-md-4" v-if="sectionFour">
+                    <div class="hover-container">
+                        <div class="hover-image" v-bind:style="{ backgroundImage: 'url(' + sectionFour.image_url + ')' }"></div>
+                    	<div v-if="sectionFour.url" class="hover-text-container hover-scale">
+                		    <div class="hover-text">
+                		        <h2>{{ sectionFour.name }}</h2>
+                		        <h4>{{ sectionFour.description }}</h4>
+                		        <router-link :to="sectionFour.url">
+                		            <h5>Learn More&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+            		            </router-link>
+            		        </div>
+                    	</div>
+                    	<div v-if="!sectionFour.url"class="hover-text-container hover-scale">
+                    		<div class="hover-text">
+                    		    <h2>{{ sectionFour.name }}</h2>
+                    		    <h4>{{ sectionFour.description }}</h4>
+                		        <a :href="sectionFour.image_url" :data-lightbox="sectionFour.name">
+                    		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                    		    </a>
+                		    </div>
+                    	</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="image-row">
+            <div class="hover-container" v-if="sectionFive">
+                <div class="hover-image max-height" v-bind:style="{ backgroundImage: 'url(' + sectionFive.image_url + ')' }"></div>
+            	<div v-if="sectionFive.url" class="hover-text-container hover-scale">
+        		    <div class="hover-text">
+        		        <h2>{{ sectionFive.name }}</h2>
+        		        <h4>{{ sectionFive.description }}</h4>
+        		        <router-link :to="sectionFive.url">
+        		            <h5>Learn More&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+    		            </router-link>
+    		        </div>
+            	</div>
+            	<div v-if="!sectionFive.url"class="hover-text-container hover-scale">
+            		<div class="hover-text">
+            		    <h2>{{ sectionFive.name }}</h2>
+            		    <h4>{{ sectionFive.description }}</h4>
+            		    <a :href="sectionFive.image_url" :data-lightbox="sectionFive.name">
+            		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+            		    </a>
+        		    </div>
+            	</div>
+            </div>
+        </div>
+        <div class="page-container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="hours-management" v-if="currentPage" v-html="currentPage.body"></div>
+                </div>
+            </div>
+            <div class="row image-row">
+                <div class="col-md-6">
+                    <div class="hover-container" v-if="sectionSix">
+                        <div class="hover-image" v-bind:style="{ backgroundImage: 'url(' + sectionSix.image_url + ')' }"></div>
+                    	<div v-if="sectionSix.url" class="hover-text-container hover-scale">
+                		    <div class="hover-text">
+                		        <h2>{{ sectionSix.name }}</h2>
+                		        <h4>{{ sectionSix.description }}</h4>
+                		        <router-link :to="sectionSix.url">
+                		            <h5>Learn More&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+            		            </router-link>
+            		        </div>
+                    	</div>
+                    	<div v-if="!sectionSix.url"class="hover-text-container hover-scale">
+                    		<div class="hover-text">
+                    		    <h2>{{ sectionSix.name }}</h2>
+                    		    <h4>{{ sectionSix.description }}</h4>
+                    	        <a :href="sectionSix.image_url" :data-lightbox="sectionSix.name">
+                    		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                    		    </a>
+                		    </div>
+                    	</div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="hover-container" v-if="sectionSeven">
+                        <div class="hover-image" v-bind:style="{ backgroundImage: 'url(' + sectionSeven.image_url + ')' }"></div>
+                    	<div v-if="sectionSeven.url" class="hover-text-container hover-scale">
+                		    <div class="hover-text">
+                		        <h2>{{ sectionSeven.name }}</h2>
+                		        <h4>{{ sectionSeven.description }}</h4>
+                		        <router-link :to="sectionSeven.url">
+            		                <h5>Learn More&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+        		                </router-link>
+            		        </div>
+                    	</div>
+                    	<div v-if="!sectionSeven.url"class="hover-text-container hover-scale">
+                    		<div class="hover-text">
+                    		    <h2>{{ sectionSeven.name }}</h2>
+                    		    <h4>{{ sectionSeven.description }}</h4>
+                    		    <a :href="sectionSeven.image_url" :data-lightbox="sectionSeven.name">
+                    		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                    		    </a>
+                		    </div>
+                    	</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row image-row">
+                <div class="col-md-12">
+                    <div class="hover-container" v-if="sectionEight">
+                        <div class="hover-image mid-height" v-bind:style="{ backgroundImage: 'url(' + sectionEight.image_url + ')' }"></div>
+                    	<div v-if="sectionEight.url" class="hover-text-container hover-scale">
+                		    <div class="hover-text">
+                		        <h2>{{ sectionEight.name }}</h2>
+                		        <h4>{{ sectionEight.description }}</h4>
+                		        <router-link :to="sectionEight.url">
+                		            <h5>Learn More&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+            		            </router-link>
+            		        </div>
+                    	</div>
+                    	<div v-if="!sectionEight.url"class="hover-text-container hover-scale">
+                    		<div class="hover-text">
+                    		    <h2>{{ sectionEight.name }}</h2>
+                    		    <h4>{{ sectionEight.description }}</h4>
+                    		    <a :href="sectionEight.image_url" :data-lightbox="sectionEight.name">
+                    		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                    		    </a>
+                		    </div>
+                    	</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="page-breadcrumb">{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;Collection Overview</p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
+    define(["Vue", "jquery", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, jQuery, moment, tz, VueMoment, Meta) {
         Vue.use(Meta);
+        Vue.use(Lightbox);
         return Vue.component("our-story-component", {
             template: template, // the variable template will be injected
             data: function() {
@@ -26,43 +206,126 @@
                     currentPage: null,
                 }
             },
-            // beforeRouteEnter (to, from, next) {
-            //     next(vm => {
-            //         // access to component instance via `vm`
-            //         vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/.json"}).then(response => {
-            //             vm.currentPage = response.data;
-            //         }, error => {
-            //             console.error("Could not retrieve data from server. Please check internet connection and try again.");
-            //             vm.$router.replace({ name: '404'});
-            //         });
-            //     })
-            // },
-            // beforeRouteUpdate (to, from, next) {
-            //     this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + ".json"}).then(response => {
-            //         this.currentPage = response.data;
-            //         console.log(this.currentPage);
-            //     }, error => {
-            //         console.error("Could not retrieve data from server. Please check internet connection and try again.");
-            //         this.$router.replace({ name: '404'});
-            //     });
-            // },
+            mounted () {
+                this.images
+            },
+            beforeRouteEnter (to, from, next) {
+                next(vm => {
+                    // access to component instance via `vm`
+                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/northpark-about-the-collection.json"}).then(response => {
+                        vm.currentPage = response.data;
+                    }, error => {
+                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                        vm.$router.replace({ name: '404'});
+                    });
+                })
+            },
+            beforeRouteUpdate (to, from, next) {
+                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-about-the-collection.json"}).then(response => {
+                    this.currentPage = response.data;
+                    console.log(this.currentPage);
+                }, error => {
+                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                    this.$router.replace({ name: '404'});
+                });
+            },
             computed: {
-                property(){
+                property() {
                     return this.$store.getters.getProperty;
                 },
-                timezone() {
-                    return this.$store.getters.getTimezone;
+                images() {
+                    var repo = _.filter(this.$store.state.results.repos, function(o) { return o.name == "Art Collection Overview" })
+                    var repo_images = _.orderBy(repo[0].images, function(o) { return o.id });
+                    return repo_images
                 },
-                hours(){
-                    var hours = _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 })
-                    return hours;
+                sectionOne(){
+                    var sectionID = 35507
+                    var sectionOne = [];
+                    _.forEach(this.images, function(value, key) {
+                        var itemID = value.id
+                        if(itemID === sectionID){
+                            sectionOne.push(value);
+                        }
+                    });
+                    return sectionOne[0];
                 },
-            },
-            methods: {
-                day_of_the_week(val_day){
-                    weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-                    return weekday[val_day];
+                sectionTwo(){
+                    var sectionID = 35508
+                    var sectionTwo = [];
+                    _.forEach(this.images, function(value, key) {
+                        var itemID = value.id
+                        if(itemID === sectionID){
+                            sectionTwo.push(value);
+                        }
+                    });
+                    return sectionTwo[0];
                 },
+                sectionThree(){
+                    var sectionID = 35509
+                    var sectionThree = [];
+                    _.forEach(this.images, function(value, key) {
+                        var itemID = value.id
+                        if(itemID === sectionID){
+                            sectionThree.push(value);
+                        }
+                    });
+                    return sectionThree[0];
+                },
+                sectionFour(){
+                    var sectionID = 35510
+                    var sectionFour = [];
+                    _.forEach(this.images, function(value, key) {
+                        var itemID = value.id
+                        if(itemID === sectionID){
+                            sectionFour.push(value);
+                        }
+                    });
+                    return sectionFour[0];
+                },
+                sectionFive(){
+                    var sectionID = 35522
+                    var sectionFive = [];
+                    _.forEach(this.images, function(value, key) {
+                        var itemID = value.id
+                        if(itemID === sectionID){
+                            sectionFive.push(value);
+                        }
+                    });
+                    return sectionFive[0];
+                },
+                sectionSix(){
+                    var sectionID = 35523
+                    var sectionSix = [];
+                    _.forEach(this.images, function(value, key) {
+                        var itemID = value.id
+                        if(itemID === sectionID){
+                            sectionSix.push(value);
+                        }
+                    });
+                    return sectionSix[0];
+                },
+                sectionSeven(){
+                    var sectionID = 35524
+                    var sectionSeven = [];
+                    _.forEach(this.images, function(value, key) {
+                        var itemID = value.id
+                        if(itemID === sectionID){
+                            sectionSeven.push(value);
+                        }
+                    });
+                    return sectionSeven[0];
+                },
+                sectionEight(){
+                    var sectionID = 35525
+                    var sectionEight = [];
+                    _.forEach(this.images, function(value, key) {
+                        var itemID = value.id
+                        if(itemID === sectionID){
+                            sectionEight.push(value);
+                        }
+                    });
+                    return sectionEight[0];
+                }
             }
         });
     });
