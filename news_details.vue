141d4744530a04380e290a9f1545031b00bee00c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-19 15:10:00 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -2,7 +2,7 @@
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="margin-90"></div>
         <div class="row" v-if="currentBlog">
-            <div class="col-md-9 margin-60">
+            <div class="col-md-8 margin-60">
                 <div class="row">
                     <div class="col-md-2">
                         <p class="details-promo-date">{{currentBlog.publish_date | moment("ddd", timezone)}}</p>
