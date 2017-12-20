ced9cda5f518eda4a700bfc298871aa85e42e0ce
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-20 16:35:56 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     
     Vue.component('vue-simple-spinner', Spinner);
     Vue.component('v-select', VueSelect.VueSelect);
-    Vue.component('vue-instagram', VueInstagram.default);
+    Vue.component('vue-instagram', VueInstagram);
     // Vue.use(VueInstagram.default)
     
     /* bootstrap app */
