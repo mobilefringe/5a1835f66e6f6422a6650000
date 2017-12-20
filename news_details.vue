e8ad15a116fb99f0a489d9748974af6615c7fb90
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-20 14:29:20 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -138,12 +138,11 @@
                 relatedPost(val_tag){
                     var main_blog = _.reverse(_.orderBy(this.blogs("main").posts, function(o) { return o.publish_date }));
                     var current_post_tag = _.join(this.currentPost.tag)
-                    console.log(current_post_tag)
                     
                     var related_blog = [];
                     _.forEach(main_blog, function(value, key) {
                         var tag_string = _.toLower(_.join(value.tag, ''));
-                        console.log(current_post_tag)
+                        console.log(tag_string)
                         if(current_post_tag === tag_string){
                             
                             related_blog.push(value);
