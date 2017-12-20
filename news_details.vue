427f61151c0d5641f6fb2194d60cd6387a3f5c77
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-20 17:12:18 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -53,8 +53,8 @@
                     </div>
                     <div class="sidebar-container" v-if="instaFeed">
                         <h5>Follow Us On Instagram</h5>
-                        <div class="row">
-                            <div class="col-md-4" v-for="item in instaFeed">
+                        <div class="insta-feed-container">
+                            <div class="insta-feed-image" v-for="item in instaFeed">
                                 <img :src="item.images.thumbnail.url"/>
                             </div>
                         </div>
