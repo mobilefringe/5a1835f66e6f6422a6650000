87ba6c4b30cba48dbea5249831b2a38103c20df6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-21 10:19:17 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -76,7 +76,7 @@
 
 <script>
     define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta", "vue-social-sharing", "vue-affix"], function(Vue, moment, tz, VueMoment, Meta, SocialSharing, VueAffix) {
-        // Vue.use(VueAffix)     
+        Vue.use(VueAffix)     
         return Vue.component("news-details-component", {
             template: template, // the variable template will be injected,
             data: function() {
