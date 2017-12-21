4a4a5e61642bc99518d7af53dc4b48d1ee954b96
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-21 15:00:50 -0500

    Northpark 1.0  /magazine.vue

--- magazine.vue
+++ magazine.vue
@@ -50,17 +50,17 @@
             mounted () {
                 this.restaurant
             },
-            beforeRouteEnter (to, from, next) {
-                next(vm => {
-                    // access to component instance via `vm`
-                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/northpark-management-hours" + ".json"}).then(response => {
-                        vm.currentPage = response.data;
-                    }, error => {
-                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                        vm.$router.replace({ name: '404'});
-                    });
-                })
-            },
+            // beforeRouteEnter (to, from, next) {
+            //     next(vm => {
+            //         // access to component instance via `vm`
+            //         vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/northpark-management-hours" + ".json"}).then(response => {
+            //             vm.currentPage = response.data;
+            //         }, error => {
+            //             console.error("Could not retrieve data from server. Please check internet connection and try again.");
+            //             vm.$router.replace({ name: '404'});
+            //         });
+            //     })
+            // },
             // beforeRouteUpdate (to, from, next) {
             //     this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-management-hours.json"}).then(response => {
             //         this.currentPage = response.data;
