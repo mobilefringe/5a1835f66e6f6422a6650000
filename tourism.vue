e5ff5f07a494b466f855649471263b10f36f0203
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-21 11:06:35 -0500

    Northpark 1.0  /tourism.vue

--- tourism.vue
+++ tourism.vue
@@ -157,7 +157,7 @@
                     return hours;
                 },
                 pageBanner() {
-                    var repo = _.filter(this.$store.state.results.repos, function(o) { return o.name == "Visit" })
+                    var repo = _.filter(this.$store.state.results.repos, function(o) { return o.name == "Tourism" })
                     var repo_images = repo[0].images[0]
                     return repo_images
                 }
