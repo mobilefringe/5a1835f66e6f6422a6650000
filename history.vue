917d71fcc8b05f27d65c51a2e043fae354873709
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-21 12:29:43 -0500

    Northpark 1.0  /history.vue

--- history.vue
+++ history.vue
@@ -55,6 +55,7 @@
             template: template, // the variable template will be injected
             data: function() {
                 return {
+                    breadcrumb: null,
                     currentPage: null,
                     history: null,
                     anniversary: null,
