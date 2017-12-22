c918739f643697410fc5c1b1b2646d16c7492a6b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-22 11:30:54 -0500

    Northpark 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <div class="margin-60"></div>
+        <div class="margin-42"></div>
         <div class="row">
             <div class="col-md-6">
                 <div class="category-select-container">
