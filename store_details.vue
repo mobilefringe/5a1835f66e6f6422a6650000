861f303e35b971928923957f7cc553b97d82a12d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-22 14:37:04 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -8,104 +8,105 @@
             </slick>
             <img v-if="!currentStore.assets" :src="currentStore.store_front_url_abs" alt="" />
         </div>
-    </div>
-    <div class="page-container">
-        <div class="margin-90 hidden-mobile"></div>
-        <div class="row" >
-            <!--<div class="col-md-12 margin-60">-->
-            <!--    <slick v-if="currentDetails" ref="slick" :options="slickOptions">-->
-            <!--        <div v-for="asset in storeAssets" >-->
-            <!--            <img :src="asset.url" alt="">-->
-            <!--        </div>-->
-            <!--    </slick>-->
-            <!--    <img v-if="!currentStore.assets" :src="currentStore.store_front_url_abs" alt="" />-->
-            <!--</div>-->
-            <div class="col-md-8">
-                <div class="details-store-info">
-                    <h2 class="details-store-name">{{currentStore.name}}</h2>
-                    <p class="details-description">{{currentStore.description}}</p>
-                    <div class="details-reservation" v-if="currentStore && currentStore.website">
-                        <div v-if="!isDine">
-                            <a class="details-link" :href="'http://' + currentStore.website" target="_blank">View Store Website&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
-                        </div>
-                        <div v-if="isDine">
-                            <h5>Reservations</h5>
-                            <a class="details-link" :href="'http://' + currentStore.website" target="_blank">Make Reservations&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
-                        </div>
-                    </div>
-                    <router-link to="/stores" active-class="active" exact>
-                        <h5 class="details-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
-                    </router-link>
-                    <hr class="visible-mobile mobile-divider">
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="sidebar">
-                    <div class="sidebar-container" v-if="currentStore && currentStore.phone">
-                        <h5 class="dine-contact-header">Contact</h5>
-                        <p>{{currentStore.phone}}</p>
-                    </div>
-                    <div class="sidebar-container" v-if="currentStore && currentStore.store_hours">
-                        <h5>Hours</h5>
-                        <ul class="details-hours-list">
-                            <li v-for="hour in hours">
-                               {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
-                                </span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="sidebar-container" v-if="currentStore && currentStore.keywords">
-                        <h5>Ammenities</h5>
-                        <p>{{currentStore.keywords}}</p>
-                    </div>
-                    <div class="sidebar-container" v-if="currentStore && currentStore.manager_name">
-                        <h5>Manager</h5>
-                        <p class="details-manager-name">{{currentStore.manager_name}}</p>
-                    </div>    
-                </div>
-                
-            </div>
-        </div>
-        <div v-if="currentStore && currentStore.total_published_promos > 0">
-            <div class="row">
-                <div class="col-md-12 details-promo-header">
-                    <h5>Upcoming Events</h5>
-                </div>
-            </div>
-            <div v-for="promo in promotions" v-if="promotions">
-                <div class="row details-promo-container">
-                    <div class="col-sm-12 col-md-6 col-md-push-6">
-                        <img :src="promo.promo_image_url_abs" class="details-promo-img" alt="">
-                    </div>
-                    <div class="col-sm-12 col-md-6 col-md-pull-6">
-                        <div class="row">
-                            <div class="col-sm-3 col-md-2">
-                                <p class="details-promo-date">{{promo.start_date | moment("ddd", timezone)}}</p>
-                                <p class="details-promo-day">{{promo.start_date | moment("D", timezone)}}</p>
-                                <p class="details-promo-date">{{promo.start_date | moment("MMM", timezone)}}</p>
+
+        <div class="page-container">
+            <div class="margin-90 hidden-mobile"></div>
+            <div class="row" >
+                <!--<div class="col-md-12 margin-60">-->
+                <!--    <slick v-if="currentDetails" ref="slick" :options="slickOptions">-->
+                <!--        <div v-for="asset in storeAssets" >-->
+                <!--            <img :src="asset.url" alt="">-->
+                <!--        </div>-->
+                <!--    </slick>-->
+                <!--    <img v-if="!currentStore.assets" :src="currentStore.store_front_url_abs" alt="" />-->
+                <!--</div>-->
+                <div class="col-md-8">
+                    <div class="details-store-info">
+                        <h2 class="details-store-name">{{currentStore.name}}</h2>
+                        <p class="details-description">{{currentStore.description}}</p>
+                        <div class="details-reservation" v-if="currentStore && currentStore.website">
+                            <div v-if="!isDine">
+                                <a class="details-link" :href="'http://' + currentStore.website" target="_blank">View Store Website&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
                             </div>
-                            <div class="col-sm-9 col-md-10">
-                                <h2 class="details-promo-name">{{promo.name}}</h2> 
-                                <h5 class="details-promo-store">{{promo.store.name}}</h5>
-                                <p class="details-promo-desc">{{ truncate(promo.description) }}</p> 
-                                <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}">
-                                    <h5 class="details-link">Read More <i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
-                                </router-link>    
+                            <div v-if="isDine">
+                                <h5>Reservations</h5>
+                                <a class="details-link" :href="'http://' + currentStore.website" target="_blank">Make Reservations&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
                             </div>
+                        </div>
+                        <router-link to="/stores" active-class="active" exact>
+                            <h5 class="details-link">View Northpark Center Map <i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                        </router-link>
+                        <hr class="visible-mobile mobile-divider">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="sidebar">
+                        <div class="sidebar-container" v-if="currentStore && currentStore.phone">
+                            <h5 class="dine-contact-header">Contact</h5>
+                            <p>{{currentStore.phone}}</p>
+                        </div>
+                        <div class="sidebar-container" v-if="currentStore && currentStore.store_hours">
+                            <h5>Hours</h5>
+                            <ul class="details-hours-list">
+                                <li v-for="hour in hours">
+                                   {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-container" v-if="currentStore && currentStore.keywords">
+                            <h5>Ammenities</h5>
+                            <p>{{currentStore.keywords}}</p>
+                        </div>
+                        <div class="sidebar-container" v-if="currentStore && currentStore.manager_name">
+                            <h5>Manager</h5>
+                            <p class="details-manager-name">{{currentStore.manager_name}}</p>
                         </div>    
                     </div>
+                    
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="page-breadcrumb">
-                    <span>{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
-                    <breadcrumbs>{{ breadcrumb }}</breadcrumbs>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;
-                    <span v-if="isNewStore"><router-link to="/new-doors">New Doors</router-link>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
-                    <span v-if="!isNewStore || isDine"><router-link to="/dine">Dine</router-link>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
-                    <span v-if="currentStore">{{currentStore.name }}</span>
-                </p>
+            <div v-if="currentStore && currentStore.total_published_promos > 0">
+                <div class="row">
+                    <div class="col-md-12 details-promo-header">
+                        <h5>Upcoming Events</h5>
+                    </div>
+                </div>
+                <div v-for="promo in promotions" v-if="promotions">
+                    <div class="row details-promo-container">
+                        <div class="col-sm-12 col-md-6 col-md-push-6">
+                            <img :src="promo.promo_image_url_abs" class="details-promo-img" alt="">
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-md-pull-6">
+                            <div class="row">
+                                <div class="col-sm-3 col-md-2">
+                                    <p class="details-promo-date">{{promo.start_date | moment("ddd", timezone)}}</p>
+                                    <p class="details-promo-day">{{promo.start_date | moment("D", timezone)}}</p>
+                                    <p class="details-promo-date">{{promo.start_date | moment("MMM", timezone)}}</p>
+                                </div>
+                                <div class="col-sm-9 col-md-10">
+                                    <h2 class="details-promo-name">{{promo.name}}</h2> 
+                                    <h5 class="details-promo-store">{{promo.store.name}}</h5>
+                                    <p class="details-promo-desc">{{ truncate(promo.description) }}</p> 
+                                    <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}">
+                                        <h5 class="details-link">Read More <i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>
+                                    </router-link>    
+                                </div>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="page-breadcrumb">
+                        <span>{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
+                        <breadcrumbs>{{ breadcrumb }}</breadcrumbs>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;
+                        <span v-if="isNewStore"><router-link to="/new-doors">New Doors</router-link>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
+                        <span v-if="!isNewStore || isDine"><router-link to="/dine">Dine</router-link>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;</span>
+                        <span v-if="currentStore">{{currentStore.name }}</span>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
