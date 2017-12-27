bd40b77e52056200b3b743f0c63aac6c138edb01
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-27 16:16:06 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -64,7 +64,7 @@
                     </div>
                 </div>
                 <div v-for="promo in promotions" v-if="promotions">
-                    <div class="row details-promo-container">
+                    <div class="row details-promo-container" v-class="details-promo-container-last : $index === (items.length-1)">
                         <div class="col-sm-12 col-md-6 col-md-push-6">
                             <img :src="promo.promo_image_url_abs" class="details-promo-img" alt="">
                         </div>
