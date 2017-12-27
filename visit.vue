dffcb696e911e0ae700d12358f5af888890a8b0a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-27 14:12:52 -0500

    Northpark 1.0  /visit.vue

--- visit.vue
+++ visit.vue
@@ -42,6 +42,7 @@
                             <p class="visit-link details-link">Learn More <i class="fa fa-angle-double-right" aria-hidden="true"></i></p>
                         </router-link>
                         <hr class="hidden-mobile">
+                        <div class="visible-mobile margin-120"></div>
                     </div>
                     <div class="" v-if="concierge">
                         <h2 class="visit-title">Concierge</h2>
@@ -50,6 +51,7 @@
                             <p class="visit-link details-link">Learn More <i class="fa fa-angle-double-right" aria-hidden="true"></i></p>
                         </router-link>
                         <hr class="hidden-mobile">
+                        <div class="visible-mobile margin-120"></div>
                     </div>
                     <div class="" v-if="giftCards">
                         <h2 class="visit-title">Northpark Gold & Gift Cards</h2>
@@ -58,6 +60,7 @@
                             <p class="visit-link details-link">Learn More <i class="fa fa-angle-double-right" aria-hidden="true"></i></p>
                         </router-link>
                         <hr class="hidden-mobile">
+                        <div class="visible-mobile margin-120"></div>
                     </div>
                     <div class="" v-if="tourism">
                         <h2 class="visit-title">Tourism</h2>
@@ -66,6 +69,7 @@
                             <p class="visit-link details-link">Learn More <i class="fa fa-angle-double-right" aria-hidden="true"></i></p>
                         </router-link>
                         <hr class="hidden-mobile">
+                        <div class="visible-mobile margin-120"></div>
                     </div>
                     <div class="" v-if="contact">
                         <h2 class="visit-title">Contact</h2>
