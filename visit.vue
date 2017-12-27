f267664f68745803b23a9391248d91e77d98ade2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-27 13:53:39 -0500

    Northpark 1.0  /visit.vue

--- visit.vue
+++ visit.vue
@@ -5,7 +5,35 @@
         </div>
         <div class="page-container">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-4 col-md-push-8">
+                    <div class="sidebar">
+                        <div class="sidebar-container">
+                            <h5>Hours</h5>
+                            <ul class="sidebar-hours-list">
+                                <li v-for="hour in hours">
+                                   {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
+                                </li>
+                            </ul> 
+                            <router-link to="/hours" active-class="active" exact>
+                                <a class="details-link">View Detailed Hours <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                            </router-link>
+                        </div>
+                        <div class="sidebar-container">
+                            <h5 class="">Find Us</h5>
+                            <p>
+                                {{property.name}}<br/>
+                                {{property.address1}}<br/>
+                                {{property.city}}, {{property.province_state}} {{property.postal_code}}<br/>
+                                {{property.contact_phone}}
+                            </p>
+                            <p>Northpark Center Concierge</p>
+                            <router-link to="" active-class="active" exact>
+                                <a class="details-link">Get Directions <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8 col-md-pull-4">
                     <div class="" v-if="valet">
                         <h2 class="visit-title">Valet & Parking</h2>
                         <div class="visit-desc" v-if="valet" v-html="truncate(valet.body)"></div>
@@ -46,34 +74,7 @@
                         </router-link>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="sidebar">
-                        <div class="sidebar-container">
-                            <h5>Hours</h5>
-                            <ul class="sidebar-hours-list">
-                                <li v-for="hour in hours">
-                                   {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
-                                </li>
-                            </ul> 
-                            <router-link to="/hours" active-class="active" exact>
-                                <a class="details-link">View Detailed Hours <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
-                            </router-link>
-                        </div>
-                        <div class="sidebar-container">
-                            <h5 class="">Find Us</h5>
-                            <p>
-                                {{property.name}}<br/>
-                                {{property.address1}}<br/>
-                                {{property.city}}, {{property.province_state}} {{property.postal_code}}<br/>
-                                {{property.contact_phone}}
-                            </p>
-                            <p>Northpark Center Concierge</p>
-                            <router-link to="" active-class="active" exact>
-                                <a class="details-link">Get Directions <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
-                            </router-link>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
             <div class="row">
                 <div class="col-md-12">
