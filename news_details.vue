16156d9d73b5d733fdb2fa385e4a4fb23dd8c0b5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-29 14:02:55 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -33,7 +33,8 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-10 offset-md-2">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-10">
                             <div v-html="currentPost.html_body"></div>
                         </div>
                     </div>
