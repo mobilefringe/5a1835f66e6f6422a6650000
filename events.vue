54eb8fedc5a9b12ee76f2bef6ac72217d59511a3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-02 09:58:53 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -14,7 +14,15 @@
                 <p v-if="!currentSelection">Sorry, there are no events that match your search.</p>
                 <div class="col-sm-12 col-md-5 col-md-push-7">
                     <!-- CALENDAR -->
-                    <v-calendar></v-calendar>
+                    <v-calendar
+                      :is-double-paned='isDoublePaned'
+                      :is-expanded='isExpanded'
+                      :nav-visibility='navVisibility'
+                      :title-position='titlePosition'
+                      :title-transition='titleTransition'
+                      :weeks-transition='weeksTransition'
+                      >
+                    </v-calendar>
                     <v-date-picker :mode='mode' v-model='selectedDate'></v-date-picker>
                     <!--<vue-datepicker-local v-model="time" type="inline"></vue-datepicker-local>-->
                     <div class="category-select-container right">
