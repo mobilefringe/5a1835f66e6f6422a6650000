53a4e83532bf22e57ea20be6b4bdbe4879cd4d31
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-02 11:09:37 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -4,7 +4,7 @@
         <div class="image-container">
             <slick ref="slick" :options="slickOptions">
                 <div v-for="banner in eventsBanners" v-if="eventsBanners">
-                    <router-link v-if="banner.url" :to="banner.url" class="">
+                    <router-link  class=""> <!-- :to="banner.url" -->
                         <img class="margin-60" :src="banner.image_url" alt="">
                     </router-link>
                 </div>
