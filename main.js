387ab66a78fc3fe1650a9e907655008b70443656
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-03 17:17:59 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     
     Vue.component('vue-simple-spinner', Spinner);
     Vue.component('v-select', VueSelect.VueSelect);
-    // Vue.component('affix', VueAffix.default)
+    Vue.component('affix', VueAffix.default)
 
     /* bootstrap app */
     const vm = new Vue({
