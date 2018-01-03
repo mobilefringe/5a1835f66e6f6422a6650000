592ffa9907ae4d971569a4454822cba8749b7719
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-03 16:53:20 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ require.config({
         'vue-select': 'https://cdn.jsdelivr.net/npm/vue-select@2.3.0/dist/vue-select.min',
         'lightbox': 'https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/js/lightbox.min',
         'vue-affix': 'https://cdn.jsdelivr.net/npm/vue-affix@0.2.4/dist/vue-affix',
-        //'v-calendar': 'https://unpkg.com/v-calendar@0.5.1/lib/v-calendar.min',
+        'v-calendar': 'https://unpkg.com/v-calendar@0.5.1/lib/v-calendar.min',
         'pdfjs-dist/build/pdf': 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.0.246/build/pdf.min'
     }
 });
