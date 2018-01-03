e3665f0985eb7d8b467de8441da1cdc2317779d5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-03 11:02:05 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -129,6 +129,29 @@
                         }
                     });
                     return holiday_events;
+                },
+                attributes() {
+                    if (!this.patterns || Object.keys(this.patterns).length === 0) return [];
+                        return [
+                            {
+                              highlight: {
+                                backgroundColor: '#ff8080', // Red
+                                borderColor: '#ff6666',
+                                borderWidth: '2px',
+                                borderStyle: 'solid',
+                              },
+                              contentStyle: {
+                                color: 'white',
+                              },
+                              dates: [
+                                {
+                                  start: new Date(thisMonthYear, thisMonth, 1),
+                                  ...this.patterns,
+                                },
+                              ],
+                            },
+                        ];
+                    },
                 }
             },
             methods: {
