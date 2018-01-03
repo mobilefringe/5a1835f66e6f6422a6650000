344004f6295089bf76b13d95613aa5a63555c4a2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-03 10:48:27 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -91,7 +91,7 @@
                         fade: true,
                         infinite: true,
                         slidesToShow: 1,
-                        speed: 500,
+                        speed: 1000,
                         // prevArrow: '.prev',
                         // nextArrow: '.next'
                     }
