4269be34df08663b01faf8a6941edaa943b6f8b0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-03 16:58:46 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ require.config({
 });
 
 require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'locales', 'moment', 'vue-meta', 'vue!search-component', 'vue-simple-spinner', 'vue-breadcrumbs', 'vue-paginate', 'vue-social-sharing', 'vue-select', 'lightbox', 'v-calendar', 'pdfjs-dist/build/pdf', 'vue-affix'], function (Vue, Vue2Filters, VueRouter, appRoutes, store, VueI18n, messages, moment, Meta, SearchComponent, Spinner, VueBreadcrumbs, VuePaginate, SocialSharing, VueSelect, Viewer, Lightbox, VCalendar, pdf, VueAffix) {
-    console.log(VueAffix);
+    console.log(pdf);
     
     Vue.use(Meta);
     Vue.use(VueRouter);
