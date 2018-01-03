494527768064522f6c35d33807c6d0d261bae3d0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-03 14:22:08 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -135,6 +135,7 @@
                         }
                     });
                     console.log(showEvents);
+                    this.currentSelection = showEvents
                 }
             },
             computed: {
