cbad9f836d0b961243161d5297fbb758b932c5ad
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-04 15:34:42 -0500

    Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -35,8 +35,7 @@ require.config({
 });
 
 require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'locales', 'moment', 'vue-meta', 'vue!search-component', 'vue-simple-spinner', 'vue-breadcrumbs', 'vue-paginate', 'vue-social-sharing', 'vue-select', 'lightbox', 'v-calendar', 'vue-affix'], function (Vue, Vue2Filters, VueRouter, appRoutes, store, VueI18n, messages, moment, Meta, SearchComponent, Spinner, VueBreadcrumbs, VuePaginate, SocialSharing, VueSelect, Lightbox, VCalendar, VueAffix) {
-    console.log(VueAffix)
-    
+
     Vue.use(Meta);
     Vue.use(VueRouter);
     Vue.use(Vue2Filters);
@@ -61,8 +60,6 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     
     Vue.component('vue-simple-spinner', Spinner);
     Vue.component('social-sharing', SocialSharing);
-    Vue.component('v-select', VueSelect.VueSelect);
-    // Vue.component('affix', VueAffix)
 
     /* bootstrap app */
     const vm = new Vue({
