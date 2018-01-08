698e4ab0a0bfd2c2e22ad79520e0558ad784c087
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-08 18:07:15 -0500

    Northpark 1.0  /promo_details.vue

--- promo_details.vue
+++ promo_details.vue
@@ -111,16 +111,16 @@
                 },
             },
             methods: {
-                checkEventType(val_eventable_type){
-                    if(this.currentEvent.eventable_type == "Store"){
-                        return true
-                    }
-                },
+                // checkEventType(val_eventable_type){
+                //     if(this.currentEvent.eventable_type == "Store"){
+                //         return true
+                //     }
+                // },
                 getStoreSlug(){
-                    if(this.currentEvent.eventable_type == "Store"){
+                    // if(this.currentEvent.eventable_type == "Store"){
                         var store_slug = "/stores/" + this.currentEvent.store.slug
                         return store_slug
-                    }    
+                    // }    
                 },
                 day_of_the_week(val_day){
                     weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
