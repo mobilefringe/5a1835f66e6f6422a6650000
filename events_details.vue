ae687f7501ffcad7d567302f15df6f341a9e7791
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-08 12:26:07 -0500

    Northpark 1.0  /events_details.vue

--- events_details.vue
+++ events_details.vue
@@ -75,8 +75,10 @@
             beforeRouteUpdate (to, from, next) {
                 this.currentEvent = this.findEventBySlug(to.params.id);
                 if (this.currentEvent === null || this.currentEvent === undefined){
+                    
                     this.$router.replace({ name: '404'});
                 }
+                console.log(this.currentEvent)
             },
             watch: {
                 currentEvent: function() {
