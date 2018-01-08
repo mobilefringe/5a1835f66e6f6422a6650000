7ddc09de734c5ada7fc14855a59aabd15e059047
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-08 13:00:11 -0500

    Northpark 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -9,8 +9,8 @@
                     
                     class="category-select" >
                 </v-select>
-                <div class="storelist-container" v-if="allStores">
-                    <div v-for="store in allStores">
+                <div class="storelist-container" v-if="currentSelection">
+                    <div v-for="store in currentSelection">
                         <p>{{store.name}}</p>
                     </div>    
                 </div>
