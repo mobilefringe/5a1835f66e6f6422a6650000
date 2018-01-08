50111c8420f13caaba68637942e9fd217545a37d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-08 10:45:19 -0500

    Northpark 1.0  /magazine.vue

--- magazine.vue
+++ magazine.vue
@@ -9,16 +9,14 @@
         <div class="row">
             <div class="col-md-8">
                 <h2>The Magazine</h2>
-                <!--<div class="row">-->
+                <div class="row">
                     <slick class="magazine-slider" ref="slick" :options="slickOptions">
                         <div v-for="item in magazines">
-                            
-                                <embed :src="item.image_url" width="500" height="375" type='application/pdf'>
-                                <h5 class="details-title">{{ item.title }}</h5>
-                            
+                            <embed :src="item.image_url" width="500" height="375" type='application/pdf'>
+                            <h5 class="details-title">{{ item.title }}</h5>
                         </div>
                     </slick>
-                <!--</div>-->
+                </div>
             </div>
             <div class="col-md-4">
                 <div class="sidebar">
