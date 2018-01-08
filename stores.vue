6f541c03f09a44d2ec649f8c022a3cde16a51f50
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-08 12:40:47 -0500

    Northpark 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -18,7 +18,7 @@
 </template>
 <!--:on-change="selectCategory()"-->
 <script>
-    define(["Vue", "jquery", "Raphael", "mm_mapsvg", "mousewheel", "vue!svg-map"], function(Vue, $, Raphael, mapSvg, mousewheel, SVGMapComponent) {
+    define(["Vue", "vue-breadcrumbs", "v-select", "jquery", "Raphael", "mm_mapsvg", "mousewheel", "vue!svg-map"], function(Vue, VueBreadcrumbs, vSelect, $, Raphael, mapSvg, mousewheel, SVGMapComponent) {
         return Vue.component("stores-component", {
             template: template, // the variable template will be injected
             data: function() {
