8ff3fd3ea733966843cdeb903ab3e338f39fc6bc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-09 12:31:22 -0500

    Northpark 1.0  /tourism.vue

--- tourism.vue
+++ tourism.vue
@@ -18,25 +18,25 @@
                     <div class="sidebar-container">
                         <h5 class="">Find Us</h5>
                         <p class="uppercase">
-                            {{property.name}}<br/>
-                            {{property.address1}}<br/>
-                            {{property.city}}, {{property.province_state}} {{property.postal_code}}<br/>
+                            {{property.name}}<br>
+                            {{property.address1}}<br>
+                            {{property.city}}, {{property.province_state}} {{property.postal_code}}<br>
                             {{property.contact_phone}}
                         </p>
-                        <p class="uppercase">Northpark Center Concierge<br/>
+                        <p class="uppercase">Northpark Center Concierge<br>
                             {{property.contact_phone}}
                         </p>
                     </div>
                     <div class="sidebar-container">
                         <router-link to="" active-class="active" exact>
-                            <a class="details-link">Get Directions <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                            <a class="details-link" href="https://www.google.com/maps/place/NorthPark+Center/@32.868225,-96.773204,15z/data=!4m5!3m4!1s0x0:0x95fc10ba98f7aad4!8m2!3d32.8680671!4d-96.7735128?hl=en-US" target="_blank">Get Directions <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
                         </router-link>
                     </div>
                 </div>
             </div>
             <div class="col-md-8 col-md-pull-4">
                 <div v-if="pageBanner">
-                    <img  class="margin-30" :src="pageBanner.image_url" alt="" />
+                    <img class="margin-30" :src="pageBanner.image_url" alt="">
                 </div>
                 <div class="" v-if="tourism">
                     <h2 class="tourism-title">Tourism</h2>
@@ -50,7 +50,7 @@
                     <div class="tourism-newsletter-container margin-30">
                         <form action="https://mobilefringe.createsend.com/t/d/s/jhithd/" method="post">
                             <label for="fieldEmail">Email</label>
-                            <input class="" id="fieldEmail" name="cm-jhithd-jhithd" type="email" required/>
+                            <input class="" id="fieldEmail" name="cm-jhithd-jhithd" type="email" required>
                             <button class="news-submit" type="submit"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
                         </form>
                     </div>
@@ -65,17 +65,17 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <label for="">First Name</label>
-                                    <input class="" id="" name="" type="text" required/>
+                                    <input class="" id="" name="" type="text" required>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="">Last Name</label>
-                                    <input class="" id="" name="" type="text" required/>
+                                    <input class="" id="" name="" type="text" required>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-12">
                                     <label for="fieldEmail">Email</label>
-                                    <input class="" id="fieldEmail" name="cm-jhithd-jhithd" type="email" required/>
+                                    <input class="" id="fieldEmail" name="cm-jhithd-jhithd" type="email" required>
                                     <button class="news-submit" type="submit"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
                                 </div>
                             </div>
@@ -99,83 +99,89 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-                <p class="page-breadcrumb">{{property.name}}&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;<breadcrumbs>{{ breadcrumb }}</breadcrumbs>&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp; Tourism</p>
+                <p class="page-breadcrumb">{{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i> <breadcrumbs>{{ breadcrumb }}</breadcrumbs> <i class="fa fa-angle-right" aria-hidden="true"></i>  Tourism</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
-        Vue.use(Meta);
-        return Vue.component("visit-component", {
-            template: template, // the variable template will be injected
-            data: function() {
-                return {
-                    breadcrumb: null,
-                    mainPage: null,
-                    tourism: null,
-                    guestRewards: null,
-                    groupVisits: null,
-                    taxFreeShopping: null,
-                    unionPay: null,
-                }
-            },
-            beforeRouteEnter (to, from, next) {
-                next(vm => {
-                    // access to component instance via `vm`
-                    //Tourism Main Page
-                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/northpark-tourism.json"}).then(response => {
-                        vm.mainPage = response.data;
-                    }, error => {
-                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                        vm.$router.replace({ name: '404'});
-                    }); 
-                })
-            },
-            beforeRouteUpdate (to, from, next) {
+
+define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function (Vue, moment, tz, VueMoment, Meta) {
+    Vue.use(Meta);
+    return Vue.component("visit-component", {
+        template: template, // the variable template will be injected
+        data: function data() {
+            return {
+                breadcrumb: null,
+                mainPage: null,
+                tourism: null,
+                guestRewards: null,
+                groupVisits: null,
+                taxFreeShopping: null,
+                unionPay: null
+            };
+        },
+        beforeRouteEnter: function beforeRouteEnter(to, from, next) {
+            next(function (vm) {
+                // access to component instance via `vm`
                 //Tourism Main Page
-                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-tourism.json"}).then(response => {
-                    this.mainPage = response.data;
-                }, error => {
+                vm.$store.dispatch('LOAD_PAGE_DATA', { url: vm.property.mm_host + "/pages/northpark-tourism.json" }).then(function (response) {
+                    vm.mainPage = response.data;
+                }, function (error) {
                     console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                    this.$router.replace({ name: '404'});
+                    vm.$router.replace({ name: '404' });
                 });
-            },
-            watch: {
-                mainPage: function() {
-                    if(this.mainPage != null){
-                        this.tourism = this.mainPage.subpages[0]
-                        this.guestRewards = this.mainPage.subpages[1]
-                        this.groupVisits = this.mainPage.subpages[2]
-                        this.taxFreeShopping = this.mainPage.subpages[3]
-                        this.unionPay = this.mainPage.subpages[4]
-                    }
-                }
-            },
-            computed: {
-                property(){
-                    return this.$store.getters.getProperty;
-                },
-                timezone() {
-                    return this.$store.getters.getTimezone;
-                },
-                hours(){
-                    var hours = _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 })
-                    return hours;
-                },
-                pageBanner() {
-                    var repo = _.filter(this.$store.state.results.repos, function(o) { return o.name == "Tourism" })
-                    var repo_images = repo[0].images[0]
-                    return repo_images
-                }
-            },
-            methods: {
-                day_of_the_week(val_day){
-                    weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-                    return weekday[val_day];
+            });
+        },
+        beforeRouteUpdate: function beforeRouteUpdate(to, from, next) {
+            var _this = this;
+
+            //Tourism Main Page
+            this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/northpark-tourism.json" }).then(function (response) {
+                _this.mainPage = response.data;
+            }, function (error) {
+                console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                _this.$router.replace({ name: '404' });
+            });
+        },
+        watch: {
+            mainPage: function mainPage() {
+                if (this.mainPage != null) {
+                    this.tourism = this.mainPage.subpages[0];
+                    this.guestRewards = this.mainPage.subpages[1];
+                    this.groupVisits = this.mainPage.subpages[2];
+                    this.taxFreeShopping = this.mainPage.subpages[3];
+                    this.unionPay = this.mainPage.subpages[4];
                 }
             }
-        });
+        },
+        computed: {
+            property: function property() {
+                return this.$store.getters.getProperty;
+            },
+            timezone: function timezone() {
+                return this.$store.getters.getTimezone;
+            },
+            hours: function hours() {
+                var hours = _.filter(this.$store.state.results.hours, function (o) {
+                    return o.store_ids == null && o.is_holiday == 0;
+                });
+                return hours;
+            },
+            pageBanner: function pageBanner() {
+                var repo = _.filter(this.$store.state.results.repos, function (o) {
+                    return o.name == "Tourism";
+                });
+                var repo_images = repo[0].images[0];
+                return repo_images;
+            }
+        },
+        methods: {
+            day_of_the_week: function day_of_the_week(val_day) {
+                weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                return weekday[val_day];
+            }
+        }
     });
-</script>
+});</script>
