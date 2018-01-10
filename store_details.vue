f1eadf7fa85195930e138ddee7c3dbf33a3da110
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-10 10:14:36 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -49,11 +49,11 @@
                         <div class="sidebar-container" v-if="currentStore && currentStore.keywords">
                             <h5>Ammenities</h5>
                             <p>{{currentStore.keywords}}</p>
-                            <ul>
-                                <li v-for="keyword in currentStore.keywords">
-                                    {{ keyword }}   
-                                </li>
-                            </ul>
+                            <!--<ul>-->
+                            <!--    <li v-for="keyword in currentStore.keywords">-->
+                            <!--        {{ keyword }}   -->
+                            <!--    </li>-->
+                            <!--</ul>-->
                         </div>
                         <div class="sidebar-container" v-if="currentStore && currentStore.manager_name">
                             <h5>Manager</h5>
