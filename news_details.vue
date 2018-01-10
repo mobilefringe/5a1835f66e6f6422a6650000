eff985611606fdfd2def685c2520b48a54b140a9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-10 15:00:11 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -31,7 +31,6 @@
                         <h5 class="blog-author">By {{currentPost.author}} | {{currentPost.publish_date | moment("MM-D-YYYY", timezone)}}</h5>
                     </div>
                     <div class="blog-details-content">
-                        <!--<img class="blog-details-image" :src="currentPost.image_url"/>-->
                         <div class="margin-60" v-html="currentPost.html_body"></div>
                         <div class="visible-mobile">
                             <social-sharing v-if="currentPost" :url="shareURL(currentPost.slug)" :title="currentPost.title" :description="currentPost.body" :quote="truncate(currentPost.body)" twitter-user="NorthParkCenter" :media="currentPost.image_url" inline-template>
