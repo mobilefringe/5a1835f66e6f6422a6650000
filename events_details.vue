f3883d25967ab49f01aea36acf70ac8d3beb7cb3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-11 10:36:02 -0500

    Northpark 1.0  /events_details.vue

--- events_details.vue
+++ events_details.vue
@@ -12,7 +12,10 @@
                             <h2 class="details-store-name">{{currentEvent.name}}</h2>
                             <h5 class="details-dates">{{ checkEventDates() }}</h5>
                             <p class="details-description">{{currentEvent.description}}</p>
-                        </div>    
+                        </div> 
+                        <div class="visible-mobile margin-30">
+                            <hr>    
+                        </div>
                     </div>
                     <div class="col-md-4">
                         <div class="sidebar">
