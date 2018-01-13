d331c599560c02ae8adf7c7505f2dde8f9e3c22c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-13 11:16:05 -0500

    Northpark 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -215,6 +215,7 @@
                     'property',
                     'timezone',
                     'findRepoByName',
+                    'findRepoBySlug','
                     'processedEvents',
                 ]),
                 eventsBanners() {
