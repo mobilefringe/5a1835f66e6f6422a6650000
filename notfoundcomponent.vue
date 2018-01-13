26d860feaf08ac1efb15c0f33010a9e0fc2a9992
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-13 17:31:45 -0500

    Northpark 1.0  /notfoundcomponent.vue

--- notfoundcomponent.vue
+++ notfoundcomponent.vue
@@ -11,8 +11,8 @@
                     <div class="sidebar-container">
                         <h5>Hours</h5>
                         <ul class="sidebar-hours-list">
-                            <li v-for="hour in hours">
-                               {{day_of_the_week(hour.day_of_week)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
+                            <li v-for="hour in getPropertyHours">
+                               {{hour.day_of_week | moment("dddd", timezone)}} - {{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}
                             </li>
                         </ul> 
                         <router-link to="/hours" active-class="active" exact>
@@ -42,7 +42,7 @@
 </template>
 
 <script>
-    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, Vuex, moment, tz, VueMoment, Meta) {
         Vue.use(Meta);
         return Vue.component("not-found-component", {
             template: template, // the variable template will be injected
@@ -50,28 +50,11 @@
                 return {"text": "The page you are looking for cannot be found."};
             },
             computed: {
-                property(){
-                    return this.$store.getters.getProperty;
-                },
-                timezone() {
-                    return this.$store.getters.getTimezone;
-                },
-                hours(){
-                    var hours = _.filter(this.$store.state.results.hours, function(o) { return o.store_ids==null && o.is_holiday==0 })
-                    return hours;
-                },
-                historyBanners() {
-                    var repo = _.filter(this.$store.state.results.repos, function(o) { return o.name == "history banners" })
-                    var repo_images = repo[0].images
-                    console.log(repo_images)
-                    return repo_images
-                },
-            },
-            methods: {
-                day_of_the_week(val_day){
-                    weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-                    return weekday[val_day];
-                },
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'getPropertyHours'
+                ])
             }
         });
     });
