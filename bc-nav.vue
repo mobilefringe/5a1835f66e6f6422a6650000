f14e145f715fc0033c03ce9fb100956bdad81ba7
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2018-01-12 22:18:48 -0500

    Northpark 1.0  /bc-nav.vue

--- bc-nav.vue
+++ bc-nav.vue
@@ -1,20 +1,18 @@
 <template>
-    <div>
-        <ul class="breadcrumb-menu">
-            <router-link tag="li" to="/stores" active-class="active" exact>
-                <a class="main-menu-link"><h5>Store Directory</h5></a>
-            </router-link>
-            <router-link tag="li" to="/dine" active-class="active" exact>
-                <a class="main-menu-link"><h5>Restaurants & Eateries</h5></a>
-            </router-link>
-            <li>
-                <a class="main-menu-link" href="https://www.amctheatres.com/movie-theatres/dallas-ft-worth/amc-northpark-15" target="_blank"><h5>AMC</h5></a>
-            </li>
-            <router-link tag="li" to="/new-doors" active-class="active" exact>
-                <a class="main-menu-link"><h5>New Doors</h5></a>
-            </router-link>
-        </ul>
-    </div>
+    <ul class="breadcrumb-menu">
+        <router-link tag="li" to="/stores" active-class="active" exact>
+            <a class="main-menu-link"><h5>Store Directory</h5></a>
+        </router-link>
+        <router-link tag="li" to="/dine" active-class="active" exact>
+            <a class="main-menu-link"><h5>Restaurants & Eateries</h5></a>
+        </router-link>
+        <li>
+            <a class="main-menu-link" href="https://www.amctheatres.com/movie-theatres/dallas-ft-worth/amc-northpark-15" target="_blank"><h5>AMC</h5></a>
+        </li>
+        <router-link tag="li" to="/new-doors" active-class="active" exact>
+            <a class="main-menu-link"><h5>New Doors</h5></a>
+        </router-link>
+    </ul>
 </template>
 
 <script>
