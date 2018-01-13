89abfea9e2d47638ad7c77fa1639048a06337d7b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-13 16:07:38 -0500

    Northpark 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -142,13 +142,13 @@
                 }
                 
                 //Stores JSON
-                var store_id = this.currentStore.id
-                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v4/northpark/stores/" + store_id + "/store_files.json"}).then(response => {
-                    this.currentDetails = response.data;
-                }, error => {
-                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                    this.$router.replace({ name: '404'});
-                });
+                // var store_id = this.currentStore.id
+                // this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v4/northpark/stores/" + store_id + "/store_files.json"}).then(response => {
+                //     this.currentDetails = response.data;
+                // }, error => {
+                //     console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                //     this.$router.replace({ name: '404'});
+                // });
             },
             mounted () {
                 this.currentStore
