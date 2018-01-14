1db27cee19f3206fee8cdc12d10f7a10de8ccd42
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-14 15:07:35 -0500

    Northpark 1.0  /page_breadcrumb.vue

--- page_breadcrumb.vue
+++ page_breadcrumb.vue
@@ -1,9 +1,8 @@
 <template>
-    
         <div class="row">
             <div class="col-md-12">
                 <p class="page-breadcrumb">
-                    {{property.name}}&nbps;<i class="fa fa-angle-right" aria-hidden="true"></i>&nbps;
+                    {{property.name}} <i class="fa fa-angle-right" aria-hidden="true"></i>
                     <breadcrumbs>{{ breadcrumb }}</breadcrumbs>
                         
                 </p>
