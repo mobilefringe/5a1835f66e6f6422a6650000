4c394929806d7084cc801c39e67b420cb0f8ce73
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-14 14:49:27 -0500

    Northpark 1.0  /np-footer.vue

--- np-footer.vue
+++ np-footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer v-show="isHomePage">
+    <footer> <!--v-show="isHomePage"-->
         <div class="page-container">
             <div class="footer-nav">
                 <div class="row">
@@ -110,7 +110,10 @@
             computed: {
                 ...Vuex.mapGetters([
                     'property'
-                ])
+                ]),
+                copyright_year() {
+                    return moment().year();
+                }
             }
         });
     });
