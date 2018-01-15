00ca8708e245b61a91180bd8d0ed12c9c127d307
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-15 15:28:22 -0500

    Northpark 1.0  /events_details.vue

--- events_details.vue
+++ events_details.vue
@@ -93,7 +93,6 @@
                         var vm = this;
                         var storeHours = [];
                         _.forEach(this.currentEvent.store.store_hours, function(value, key) {
-                            console.log(vm.findHourById(value))
                             storeHours.push(vm.findHourById(value));
                         });
                         this.store_hours = storeHours;
