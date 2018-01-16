166d79fab8a33f18aac80943bf5f28d543d8fe4d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-16 13:59:21 -0500

    Northpark 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -105,9 +105,11 @@
                     console.log(this.$route.path)
                     if(this.$route.path == "/pages/northpark-parking-valet-page" || this.$route.path == "/pages/northpark-concierge-services" || this.$route.path == "/pages/northpark-northpark-gold-gift-cards" || this.$route.path == "/pages/northpark-contact-us") {
                         this.visitSubPage = true;
+                        
                     } else {
                         this.visitSubPage = false;
                     }
+                    console.log(this.visitSubPage)
                 }    
             },
             computed: {
