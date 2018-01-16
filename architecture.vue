16c28e0fdc330ddd9a33108040dad7dc52fd20f9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-16 16:43:06 -0500

    Northpark 1.0  /architecture.vue

--- architecture.vue
+++ architecture.vue
@@ -281,6 +281,7 @@
                 loadData: async function() {
                     try{
                         let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/northpark-architecture.json"})]);
+                        this.currentPage = response.data;
                     }
                     catch (e){
                         console.log("Error loading data: " + e.message);    
