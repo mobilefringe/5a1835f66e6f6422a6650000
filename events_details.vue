427348634b296b98e95629a7f2d69c859205a519
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-17 10:19:57 -0500

    Northpark 1.0  /events_details.vue

--- events_details.vue
+++ events_details.vue
@@ -12,8 +12,6 @@
                             <h2 class="details-store-name">{{currentEvent.name}}</h2>
                             <h5 class="details-dates" v-if="isMultiDayEvent(currentEvent)">{{ currentEvent.start_date | moment("dddd, MMMM D, YYYY", timezone)}} to {{ currentEvent.end_date | moment("dddd, MMMM D, YYYY", timezone)}}</h5>
                             <h5 class="details-dates" v-else>{{ currentEvent.start_date | moment("dddd, MMMM D, YYYY", timezone)}}</h5>
-                                
-                            <h5 class="details-dates">{{ checkEventDates() }}</h5>
                             <p class="details-description">{{currentEvent.description}}</p>
                         </div> 
                         <div class="visible-mobile margin-30">
@@ -113,18 +111,6 @@
                     } 
                     else {
                       return true
-                    }
-                },
-                checkEventDates(currentEvent){
-                    var timezone = this.timezone
-                    var start_date = moment(this.currentEvent.start_date).tz(timezone).format("MM-DD-YYYY")
-                    var end_date = moment(this.currentEvent.end_date).tz(timezone).format("MM-DD-YYYY")
-                    if(start_date === end_date){
-                        var one_day_event = moment(start_date).format("dddd, MMMM D, YYYY")
-                        return one_day_event
-                    } else {
-                        var multi_day_event = moment(start_date).format("dddd, MMMM D, YYYY") + " to " + moment(end_date).format("dddd, MMMM D, YYYY")
-                        return multi_day_event
                     }
                 }
             }
