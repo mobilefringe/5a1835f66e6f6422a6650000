e8da8aaa52cf2c51b84988946b1d7b30b3cbd197
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-20 16:43:34 -0500

    Northpark 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -54,7 +54,8 @@
 </template>
 
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", "v-select"], function (Vue, Vuex, moment, tz, VueMoment, Meta, VueBreadcrumbs, vSelect) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", "v-select", "vue-lazy-load"], function (Vue, Vuex, moment, tz, VueMoment, Meta, VueBreadcrumbs, vSelect, VueLazyload) {
+        Vue.use(VueLazyload);
       return Vue.component("dine-component", {
         template: template, // the variable template will be injected
         data: function () {
