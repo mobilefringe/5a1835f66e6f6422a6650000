e72daee506d95a4db22e81fd42f005bd56f23baa
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-22 14:50:29 -0500

    Northpark 1.0  /story_photo_gallery.vue

--- story_photo_gallery.vue
+++ story_photo_gallery.vue
@@ -1,44 +1,51 @@
 <template>
-  <div>
-    <np-loader v-if="!dataLoaded"></np-loader>
-    <div v-if="dataLoaded" class="page-container" v-cloak>
-      <!-- without an outer container div this component template will not render -->
-      <div class="margin-60"></div>
-      <div class="row margin-30">
-        <div class="col-md-6">
-          <div class="category-select-container">
-            <v-select v-model="selected" :options="categoryOptions" :searchable="false" class="category-select" :on-change="selectCategory()"></v-select>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="launch-gallery-container" v-if="currentSelection" v-for="(item, index) in currentSelection" :class="{ 'active': index === 0 }">
-            <a :href="item.image_url" :data-lightbox="currentSelection">
-              <h5>Launch Slideshow</h5>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="row margin-60">
-        <div class="col-md-12">
-          <p v-if="!currentSelection">Sorry, there are currently no images to display.</p>
-        </div>
-        <div class="col-sm-4 col-md-3" v-if="currentSelection" v-for="item in currentSelection">
-          <div class="gallery-item-container">
-            <a :href="item.image_url" :data-lightbox="item">
-              <div class="gallery-image-container">
-                <img v-lazy="item.image_url" />
-              </div>
-              <div class="gallery-content-container" v-if="item.name">
-                <h5 class="art-gallery-title">{{item.name}}</h5>
-                <h5 class="art-gallery-artist">{{item.description}}</h5>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <page-breadcrumb></page-breadcrumb>
+    <div> <!-- without an outer container div this component template will not render -->
+        <np-loader v-if="!dataLoaded"></np-loader>
+        <transition name="fade">
+            <div v-if="dataLoaded" class="page-container" v-cloak>
+                <div class="margin-60"></div>
+                <div class="row margin-30">
+                    <div class="col-md-6">
+                        <div class="category-select-container">
+                            <v-select 
+                                v-model="selected" 
+                                :options="categoryOptions" 
+                                :searchable="false" 
+                                class="category-select" 
+                                :on-change="selectCategory()"
+                            ></v-select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="launch-gallery-container" v-if="currentSelection" v-for="(item, index) in currentSelection" :class="{ 'active': index === 0 }">
+                            <a :href="item.image_url" :data-lightbox="currentSelection">
+                                <h5>Launch Slideshow</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row margin-60">
+                    <div class="col-md-12">
+                        <p v-if="!currentSelection">Sorry, there are currently no images to display.</p>
+                    </div>
+                    <div class="col-sm-4 col-md-3" v-if="currentSelection" v-for="item in currentSelection">
+                        <div class="gallery-item-container">
+                            <a :href="item.image_url" :data-lightbox="item">
+                                <div class="gallery-image-container">
+                                    <img v-lazy="item.image_url" />
+                                </div>
+                                <div class="gallery-content-container" v-if="item.name">
+                                    <h5 class="art-gallery-title">{{item.name}}</h5>
+                                    <h5 class="art-gallery-artist">{{item.description}}</h5>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <page-breadcrumb></page-breadcrumb>
+            </div>
+        </transition>
     </div>
-  </div>
 </template>
 
 <script>
