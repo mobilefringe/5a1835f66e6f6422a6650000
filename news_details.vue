6d40fe069e1c17f4517e37465c53e475e489f902
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-22 14:02:02 -0500

    Northpark 1.0  /news_details.vue

--- news_details.vue
+++ news_details.vue
@@ -1,107 +1,101 @@
 <template>
-  <div>
-    <np-loader v-if="!dataLoaded"></np-loader>
-    <div v-if="currentPost && dataLoaded" class="page-container" v-cloak>
-      <!-- without an outer container div this component template will not render -->
-      <div class="margin-90"></div>
-      <div class="row" v-if="currentPost">
-        <div class="col-md-8 margin-60">
-          <div id="currentPost">
-            <div class="blog-details-date">
-              <p class="details-promo-date">{{currentPost.publish_date | moment("ddd", timezone)}}</p>
-              <p class="details-promo-day">{{currentPost.publish_date | moment("D", timezone)}}</p>
-              <p class="details-promo-date">{{currentPost.publish_date | moment("MMM", timezone)}}</p>
-            </div>
-            <div class="blog-details-title">
-              <div class="hidden-mobile">
-                <social-sharing v-if="currentPost" :url="shareURL(currentPost.slug)" :title="currentPost.title" :description="currentPost.body"
-                  :quote="truncate(currentPost.body)" twitter-user="NorthParkCenter" :media="currentPost.image_url" inline-template>
-                  <div class="blog-social-share">
-                    <h5>Share</h5>
-                    <network network="facebook">
-                      <i class="fa fa-facebook-square"></i>
-                    </network>
-                    <network network="twitter">
-                      <i class="fa fa-twitter-square"></i>
-                    </network>
-                    <network network="pinterest">
-                      <i class="fa fa-pinterest-square"></i>
-                    </network>
-                  </div>
-                </social-sharing>
-              </div>
-              <h5 class="blog-details-category">{{ tagString(currentPost.tag) }}</h5>
-              <h2 class="blog-details-name">{{currentPost.title}}</h2>
-              <h5 class="blog-details-author">By {{currentPost.author}} | {{currentPost.publish_date | moment("MM-D-YYYY", timezone)}}</h5>
-            </div>
-            <div class="blog-details-content">
-              <div class="margin-60 blog-details-body" v-html="currentPost.html_body"></div>
-              <div class="visible-mobile">
-                <social-sharing v-if="currentPost" :url="shareURL(currentPost.slug)" :title="currentPost.title" :description="currentPost.body"
-                  :quote="truncate(currentPost.body)" twitter-user="NorthParkCenter" :media="currentPost.image_url" inline-template>
-                  <div class="blog-social-share">
-                    <network network="facebook">
-                      <i class="fa fa-facebook-square"></i>
-                    </network>
-                    <network network="twitter">
-                      <i class="fa fa-twitter-square"></i>
-                    </network>
-                    <network network="pinterest">
-                      <i class="fa fa-pinterest-square"></i>
-                    </network>
-                    <h5>Share</h5>
-                  </div>
-                </social-sharing>
-              </div>
-            </div>
-          </div>
-          <hr class="visible-mobile">
-        </div>
-        <div class="col-md-4">
-          <affix relative-element-selector="#currentPost">
-            <div class="sidebar">
-              <div class="sidebar-container" v-if="currentPost && relatedPost">
-                <h5 class="related-post-title">Related Blogs</h5>
-                <router-link :to="{ name: 'newsDetails', params: { id: relatedPost.slug }}">
-                  <div class="related-post-img" v-bind:style="{ backgroundImage: 'url(' + relatedPost.image_url + ')' }"></div>
-                  <p>{{relatedPost.title }}</p>
-                </router-link>
-                <div class="visible-mobile margin-90"></div>
-                <hr class="visible-mobile">
-              </div>
-              <div class="sidebar-container">
-                <h5 class="related-post-title">Get Connected</h5>
-                <p class="sidebar-newsletter-info">Subscribe to our newsletter and receive insider information on events, exclusive sales, special offers and
-                  much more.</p>
-                <div class="blog-newsletter-container">
-                  <form action="https://mobilefringe.createsend.com/t/d/s/jhithd/" method="post">
-                    <input class="" id="fieldEmail" name="cm-jhithd-jhithd" type="email" required placeholder="Enter Your Email" />
-                    <button class="news-submit" type="submit">
-                      <i class="fa fa-angle-right" aria-hidden="true"></i>
-                    </button>
-                  </form>
+    <div> <!-- without an outer container div this component template will not render -->
+        <np-loader v-if="!dataLoaded"></np-loader>
+        <transition name="fade">
+            <div v-if="currentPost && dataLoaded" class="page-container" v-cloak>
+                <div class="margin-90"></div>
+                <div class="row" v-if="currentPost">
+                    <div class="col-md-8 margin-60">
+                        <div id="currentPost">
+                            <div class="blog-details-date">
+                                <p class="details-promo-date">{{currentPost.publish_date | moment("ddd", timezone)}}</p>
+                                <p class="details-promo-day">{{currentPost.publish_date | moment("D", timezone)}}</p>
+                                <p class="details-promo-date">{{currentPost.publish_date | moment("MMM", timezone)}}</p>
+                            </div>
+                            <div class="blog-details-title">
+                                <div class="hidden-mobile">
+                                    <social-sharing v-if="currentPost" :url="shareURL(currentPost.slug)" :title="currentPost.title" :description="currentPost.body" :quote="truncate(currentPost.body)" twitter-user="NorthParkCenter" :media="currentPost.image_url" inline-template>
+                                        <div class="blog-social-share">
+                                            <h5>Share</h5>
+                                            <network network="facebook">
+                                                <i class="fa fa-facebook-square"></i>
+                                            </network>
+                                            <network network="twitter">
+                                                <i class="fa fa-twitter-square"></i>
+                                            </network>
+                                            <network network="pinterest">
+                                                <i class="fa fa-pinterest-square"></i>
+                                            </network>
+                                        </div>
+                                    </social-sharing>
+                                </div>
+                                <h5 class="blog-details-category">{{ tagString(currentPost.tag) }}</h5>
+                                <h2 class="blog-details-name">{{currentPost.title}}</h2>
+                                <h5 class="blog-details-author">By {{currentPost.author}} | {{currentPost.publish_date | moment("MM-D-YYYY", timezone)}}</h5>
+                            </div>
+                            <div class="blog-details-content">
+                                <div class="margin-60 blog-details-body" v-html="currentPost.html_body"></div>
+                                <div class="visible-mobile">
+                                    <social-sharing v-if="currentPost" :url="shareURL(currentPost.slug)" :title="currentPost.title" :description="currentPost.body" :quote="truncate(currentPost.body)" twitter-user="NorthParkCenter" :media="currentPost.image_url" inline-template>
+                                        <div class="blog-social-share">
+                                            <network network="facebook">
+                                                <i class="fa fa-facebook-square"></i>
+                                            </network>
+                                            <network network="twitter">
+                                                <i class="fa fa-twitter-square"></i>
+                                            </network>
+                                            <network network="pinterest">
+                                                <i class="fa fa-pinterest-square"></i>
+                                            </network>
+                                            <h5>Share</h5>
+                                        </div>
+                                    </social-sharing>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="visible-mobile">
+                    </div>
+                    <div class="col-md-4">
+                        <affix relative-element-selector="#currentPost">
+                            <div class="sidebar">
+                                <div class="sidebar-container" v-if="currentPost && relatedPost">
+                                    <h5 class="related-post-title">Related Blogs</h5>
+                                    <router-link :to="{ name: 'newsDetails', params: { id: relatedPost.slug }}">
+                                        <div class="related-post-img" v-bind:style="{ backgroundImage: 'url(' + relatedPost.image_url + ')' }"></div>
+                                        <p>{{relatedPost.title }}</p>
+                                    </router-link>
+                                    <div class="visible-mobile margin-90"></div>
+                                    <hr class="visible-mobile">
+                                </div>
+                                <div class="sidebar-container">
+                                    <h5 class="related-post-title">Get Connected</h5>
+                                    <p class="sidebar-newsletter-info">Subscribe to our newsletter and receive insider information on events, exclusive sales, special offers and much more.</p>
+                                    <div class="blog-newsletter-container">
+                                        <form action="https://mobilefringe.createsend.com/t/d/s/jhithd/" method="post">
+                                            <input class="" id="fieldEmail" name="cm-jhithd-jhithd" type="email" required placeholder="Enter Your Email" />
+                                            <button class="news-submit" type="submit"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
+                                        </form>
+                                    </div>
+                                    <div class="visible-mobile margin-90"></div>
+                                    <hr class="visible-mobile">
+                                </div>
+                                <div class="sidebar-container" v-if="instaFeed">
+                                    <h5 class="related-post-title">Follow Us On Instagram</h5>
+                                    <div class="insta-feed-container">
+                                        <div class="insta-feed-image" v-for="item in instaFeed">
+                                            <a :href="item.link" target="_blank"><img :src="item.images.thumbnail.url"/></a>
+                                        </div>
+                                    </div>
+                                    <div class="visible-mobile margin-90"></div>
+                                </div>
+                            </div>
+                        </affix>
+                    </div>
                 </div>
-                <div class="visible-mobile margin-90"></div>
-                <hr class="visible-mobile">
-              </div>
-              <div class="sidebar-container" v-if="instaFeed">
-                <h5 class="related-post-title">Follow Us On Instagram</h5>
-                <div class="insta-feed-container">
-                  <div class="insta-feed-image" v-for="item in instaFeed">
-                    <a :href="item.link" target="_blank">
-                      <img :src="item.images.thumbnail.url" />
-                    </a>
-                  </div>
-                </div>
-                <div class="visible-mobile margin-90"></div>
-              </div>
+                <page-breadcrumb></page-breadcrumb>
             </div>
-          </affix>
-        </div>
-      </div>
-      <page-breadcrumb></page-breadcrumb>
+        </transition>
     </div>
-  </div>
 </template>
 
 <script>
