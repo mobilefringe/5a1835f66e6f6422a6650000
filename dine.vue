ce12b080146f38d816d75a19d2083051e972c0cc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-23 13:42:45 -0500

    Northpark 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -18,7 +18,8 @@
                         <div class="col-md-12">
                             <div class="dine-image-container">
                                 <div class="hover-container">
-                                    <img v-if="restaurant.image_url" v-lazy="restaurant.image_url" :alt="restaurant.name" />
+                                    <div class="hover-image" v-lazy:background-image="estaurant.image_url"></div>
+                                    <!--<img v-if="restaurant.image_url" v-lazy="restaurant.image_url" :alt="restaurant.name" />-->
                                     <div class="hover-text-container hover-scale">
                                         <div class="hover-text">
                                             <router-link :to="{ name: 'storeDetails', params: { id: restaurant.slug }}" class="dine-link">
