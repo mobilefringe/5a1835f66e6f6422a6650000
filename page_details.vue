24708ffa74c00c5b488b961c52c99172326a0a32
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-20 16:29:25 -0400

    Northpark 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -46,10 +46,9 @@
                         <div class="col-md-8 col-md-pull-4">
                             <h2 class="page_title" v-html="currentPage.title"></h2>
                             <hr>
-                            <div v-if="contactUs" class="contact-page-body" v-html="currentPage.body"></div>
-                            <div v-else v-html="currentPage.body"></div>
-                            <div v-if="hours" class="hours-page-body" v-html="currentPage.body"></div>
-                            <div v-else v-html="currentPage.body"></div>
+                            <div v-if="contactUs" :class="{ contact-page-body: contactUs, hours-page-body: hours }" v-html="currentPage.body"></div>
+                            <!--<div v-if="hours" class="hours-page-body" v-html="currentPage.body"></div>-->
+                            <!--<div v-else v-html="currentPage.body"></div>-->
                         </div>
                     </div>
                     <div v-if="visitSubPage" class="row">
