7768c1123e562e4ffecb74897f8ba57d5cb10648
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-20 15:37:49 -0400

    Northpark 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -102,19 +102,12 @@
                     this.updateCurrentPage(this.$route.params.id);
                     if (this.$route.path == "/pages/northpark-parking-valet-page" || this.$route.path == "/pages/northpark-concierge-services" || this.$route.path == "/pages/northpark-northpark-gold-gift-cards" || this.$route.path == "/pages/northpark-contact-us") {
                         this.visitSubPage = true;
-                    } else {
-                        this.visitSubPage = false;
                     }
                     if (this.$route.path == "/visit/northpark-contact-us") {
                         this.contactUs = true;    
-                    } else {
-                        this.contactUs = false;    
                     }
-                    
                     if (this.$route.path == "/pages/northpark-holiday-hours") {
-                        this.hours = true;    
-                    } else {
-                        this.hours = false;    
+                        this.hours = true; 
                     }
                 }
             },
@@ -145,7 +138,6 @@
                                 this.hours = true;   
                             });
                         }
-                        
                         _this.dataLoaded = true;
                     }, function (error) {
                         console.error( "Could not retrieve data from server. Please check internet connection and try again.");
