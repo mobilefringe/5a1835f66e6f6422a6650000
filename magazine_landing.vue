49e135ded02f0f55ec6e7c902d7dcf3f5b0c3812
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-22 12:03:14 -0400

    Northpark 1.0  /magazine_landing.vue

--- magazine_landing.vue
+++ magazine_landing.vue
@@ -306,7 +306,7 @@
             methods: {
                 loadData: async function () {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/northpark-architecture.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/northpark-the-magazine-overview.json"})]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
